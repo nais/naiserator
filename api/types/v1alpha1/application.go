90d6b3b961bd0c5000bfcaa7620d5cec28d66171
Johnny Horvi <johnny@horvi.no>
2018-08-28 08:43:43 +0200

    cleaned up structure

--- api/types/v1alpha1/application.go
+++ api/types/v1alpha1/application.go
@@ -1,6 +1,9 @@
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
 
 type ApplicationSpec struct {
 	Team string `json:"team"`
@@ -11,6 +14,10 @@ type Application struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec ApplicationSpec `json:"spec"`
+}
+
+func (in *Application) GetObjectKind() schema.ObjectKind {
+	return in
 }
 
 type ApplicationList struct {
