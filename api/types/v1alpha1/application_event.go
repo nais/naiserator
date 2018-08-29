6693395e4c1a37d3fe994f1961bc768f141ff3d1
Kim Tore Jensen <kimtjen@gmail.com>
2018-08-29 15:15:23 +0200

    Code cleanup and counter-proof of concept
    We cannot compare resourceVersion to its cached version, as updating the
    cache will also give the resource a new resourceVersion.

---
+++ api/types/v1alpha1/application_event.go
@@ -0,0 +1,26 @@
+package v1alpha1
+
+import (
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (in *Application) GenerateErrorEvent(action string, message string) *corev1.Event {
+	return &corev1.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "naiserator-event",
+			Namespace:    in.Namespace,
+		},
+		Action:              action,
+		ReportingInstance:   "naiserator",
+		Reason:              "naiserator",
+		ReportingController: "naiserator",
+		InvolvedObject:      in.GetObjectReference(),
+		Message:             message,
+		EventTime: metav1.MicroTime{
+			Time: time.Now(),
+		},
+	}
+}
--- naiserator.go
+++ naiserator.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/nais/naiserator/api/types/v1alpha1"
 	clientV1Alpha1 "github.com/nais/naiserator/clientset/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -14,60 +15,83 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+// Kubernetes metadata annotation key used to store the version of the successfully processed resource.
+const LAST_SUCCESSFUL_RESOURCE_VERSION_KEY = "nais.io/lastSuccessfulResourceVersion"
+
+func needsUpdate(app *v1alpha1.Application) bool {
+	return app.Annotations[LAST_SUCCESSFUL_RESOURCE_VERSION_KEY] != app.ResourceVersion
+}
+
+func reportEvent(event *corev1.Event, c kubernetes.Interface) (*corev1.Event, error) {
+	return c.CoreV1().Events(event.Namespace).Create(event)
+}
+
+func createService(app *v1alpha1.Application, clientSet kubernetes.Interface) (*corev1.Service, error) {
+	blockOwnerDeletion := true
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{{
+				Kind:               app.Kind,
+				Name:               app.Name,
+				APIVersion:         app.APIVersion,
+				UID:                app.UID,
+				BlockOwnerDeletion: &blockOwnerDeletion,
+			}}},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port: 69,
+				},
+			},
+		},
+	}
+
+	return clientSet.CoreV1().Services(app.Namespace).Create(svc)
+}
+
+func updateAnnotation(app *v1alpha1.Application, appClient clientV1Alpha1.NaisV1Alpha1Interface) (*v1alpha1.Application, error) {
+	if app.Annotations == nil {
+		app.Annotations = make(map[string]string)
+	}
+	app.Annotations[LAST_SUCCESSFUL_RESOURCE_VERSION_KEY] = app.ResourceVersion
+	return appClient.Applications(app.Namespace).Update(app)
+}
+
 func add(app *v1alpha1.Application, clientSet kubernetes.Interface, appClient clientV1Alpha1.NaisV1Alpha1Interface) {
-	fmt.Println("added", app.Name)
+	var err error
+
+	glog.Infof("Start processing application '%s'", app.Name)
 
 	if !needsUpdate(app) {
-		fmt.Println("nothing changed, skipping...")
+		glog.Info("Application has not changed since last successful iteration, skipping.")
 		return
 	}
 
-	fmt.Println("updating...")
-	//app.GenerateName = "test"
-	//app.Annotations["last-succcessful-resourceversion"] = app.ResourceVersion
-
-	result, err := appClient.Applications(app.Namespace).Update(app)
+	_, err = createService(app, clientSet)
 	if err != nil {
-		fmt.Println("error when updating annotation:", err)
-		fmt.Printf("%+v\n", result)
+		glog.Errorf("While creating service: %s", err)
+		ev := app.GenerateErrorEvent("createService", err.Error())
+		_, err = reportEvent(ev, clientSet)
+		if err != nil {
+			glog.Errorf("Additionally, while creating an event for this error, another error occurred: %s", err)
+		}
+		return
 	}
-	//blockOwnerDeletion := true
-	//svc := &k8score.Service{
-	//	TypeMeta: metav1.TypeMeta{
-	//		Kind:       "Service",
-	//		APIVersion: "v1",
-	//	},
-	//	ObjectMeta: metav1.ObjectMeta{
-	//		Name: app.Name,
-	//		Namespace: "default",
-	//		OwnerReferences: []metav1.OwnerReference{{
-	//			Kind:               "Application",
-	//			Name:               app.Name,
-	//			APIVersion:         "v1alpha1",
-	//			UID:                app.UID,
-	//			BlockOwnerDeletion: &blockOwnerDeletion,
-	//		}}},
-	//	Spec: k8score.ServiceSpec{
-	//		Ports: []k8score.ServicePort{{Port: 69}},
-	//	},
-	//}
-	//
-	//_, e := clientSet.CoreV1().Services("default").Create(svc)
-	//
-	//event := k8score.Event{ObjectMeta: metav1.ObjectMeta{GenerateName: "event"}, Action:"bananarama", ReportingInstance: "what?", Reason: "because", ReportingController:  "naiserator", InvolvedObject: app.GetObjectReference(), Message: "hallo", EventTime: metav1.MicroTime{Time: time.Now()}}
-	//
-	//_, err := clientSet.CoreV1().Events("default").Create(&event)
-	//if err != nil {
-	//	fmt.Println("error with event", err.Error())
-	//}
-	//
-	//if e != nil {
-	//	fmt.Println(e.Error())
-	//}
-}
+	glog.Info("Successfully created service.")
 
-func needsUpdate(app *v1alpha1.Application) bool {
-	return app.Annotations["last-successful-resourceversion"] != app.ResourceVersion
+	_, err = updateAnnotation(app, appClient)
+	if err != nil {
+		glog.Errorf("While updating annotation: %s", err)
+		return
+	}
+
+	glog.Info("Successfully processed application.")
 }
 
 func WatchResources(clientSet clientV1Alpha1.NaisV1Alpha1Interface, genericClient kubernetes.Interface) cache.Store {
