51f528e515bbedc697e21a2b2c07fad7ddc14c6f
Kim Tore Jensen <kimtjen@gmail.com>
2018-08-30 11:21:13 +0200

    Import nais manifest spec from naisd code

--- api/types/v1alpha1/application.go
+++ api/types/v1alpha1/application.go
@@ -6,8 +6,66 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+type Healthcheck struct {
+	Liveness  Probe `json:"liveness"`
+	Readiness Probe `json:"readiness"`
+}
+
+type Ingress struct {
+	Disabled bool `json:"disabled"`
+}
+
+type IstioConfig struct {
+	Enabled bool `json:"enabled"`
+}
+
+type Probe struct {
+	Path             string `json:"path"`
+	InitialDelay     int    `json:"initialDelay"`
+	PeriodSeconds    int    `json:"periodSeconds"`
+	FailureThreshold int    `json:"failureThreshold"`
+	Timeout          int    `json:"timeout"`
+}
+
+type PrometheusConfig struct {
+	Enabled bool   `json:"enabled"`
+	Port    string `json:"port"`
+	Path    string `json:"path"`
+}
+
+type Replicas struct {
+	Min                    int `json:"min"`
+	Max                    int `json:"max"`
+	CpuThresholdPercentage int `yaml:"cpuThresholdPercentage"`
+}
+
+type ResourceList struct {
+	Cpu    string `json:"cpu"`
+	Memory string `json:"memory"`
+}
+
+type ResourceRequirements struct {
+	Limits   ResourceList `json:"limits"`
+	Requests ResourceList `json:"requests"`
+}
+
+// ApplicationSpec used to be called nais manifest.
 type ApplicationSpec struct {
-	Team string `json:"team"`
+	Healthcheck     Healthcheck          `json:"healthcheck"`
+	Image           string               `json:"image"`
+	Ingress         Ingress              `json:"ingress"`
+	Istio           IstioConfig          `json:"istio"`
+	LeaderElection  bool                 `json:"leaderElection"`
+	Logformat       string               `json:"logformat"`
+	Logtransform    string               `json:"logtransform"`
+	Port            int                  `json:"port"`
+	PreStopHookPath string               `json:"preStopHookPath"`
+	Prometheus      PrometheusConfig     `json:"prometheus"`
+	Replicas        Replicas             `json:"replicas"`
+	Resources       ResourceRequirements `json:"resources"`
+	Secrets         bool                 `json:"secrets"`
+	Team            string               `json:"team"`
+	WebProxy        bool                 `json:"webproxy"`
 }
 
 type Application struct {
