c40e2798bf333427258db820f84d5a1e4ad72ed4
Johnny Horvi <johnny@horvi.no>
2018-09-20 10:15:49 +0200

    add explicit go get of code-generator binaries

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -9,6 +9,9 @@ NAISERATOR_GOPATH=${GOPATH}/src/github.com/nais/naiserator
 
 # Since code-generator doesn't support go modules yet, we work around this
 # by copying the repo files into GOPATH, generating files, and moving it back.
+# https://github.com/kubernetes/kubernetes/issues/67566
+
+go get k8s.io/code-generator
 
 rm -rf ${NAISERATOR_GOPATH}/*
 cp -r ./* ${NAISERATOR_GOPATH}/
