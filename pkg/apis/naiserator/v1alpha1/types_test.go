1cc5882529eccc95fe485d080a0e5372050a2a7a
Kim Tore Jensen <kimtjen@gmail.com>
2018-09-21 14:19:28 +0200

    Application: separate type tests from package

--- pkg/apis/naiserator/v1alpha1/types_test.go
+++ pkg/apis/naiserator/v1alpha1/types_test.go
@@ -1,15 +1,17 @@
-package v1alpha1
+package v1alpha1_test
 
 import (
+	"testing"
+
+	"github.com/nais/naiserator/pkg/apis/naiserator/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 )
 
 func TestApplication_Hash(t *testing.T) {
-	a1, err := Application{Spec: ApplicationSpec{Team: "banan"}}.Hash()
-	a2, _ := Application{Spec: ApplicationSpec{Team: "banan"}, ObjectMeta: v1.ObjectMeta{Annotations: map[string]string{"a": "b"}}}.Hash()
-	a3, _ := Application{Spec: ApplicationSpec{Team: "banan"}, ObjectMeta: v1.ObjectMeta{Labels: map[string]string{"a": "b"}}}.Hash()
+	a1, err := v1alpha1.Application{Spec: v1alpha1.ApplicationSpec{Team: "banan"}}.Hash()
+	a2, _ := v1alpha1.Application{Spec: v1alpha1.ApplicationSpec{Team: "banan"}, ObjectMeta: v1.ObjectMeta{Annotations: map[string]string{"a": "b"}}}.Hash()
+	a3, _ := v1alpha1.Application{Spec: v1alpha1.ApplicationSpec{Team: "banan"}, ObjectMeta: v1.ObjectMeta{Labels: map[string]string{"a": "b"}}}.Hash()
 
 	assert.NoError(t, err)
 	assert.Equal(t, a1, a2, "matches, as annotations is ignored")
