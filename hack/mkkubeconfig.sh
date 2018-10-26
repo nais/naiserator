bc10ab5603fda0d60066f66a3bd381d55186f4e4
Kim Tore Jensen <kimtjen@gmail.com>
2018-10-26 14:23:20 +0200

    mkkubeconfig: create compound kubeconfig with all four clusters [skip ci]

--- hack/mkkubeconfig.sh
+++ hack/mkkubeconfig.sh
@@ -27,23 +27,30 @@ if [ "$subject" == "" ] || [ "$subject" == "--help" ] || [ "$subject" == "-h" ];
   help
 fi
 
-cluster_name=`kubectl config view --minify -o=jsonpath='{.clusters[0].name}'`
-api_server_url=`kubectl config view --minify -o=jsonpath='{.clusters[0].cluster.server}'`
-
-print Creating a TLS certificate ${cluster_name} with CN="${subject}"
-print Kubernetes API server is ${api_server_url}
-
-key=`mktemp`
-cert=`mktemp`
-csr=`mktemp`
-kubecsr=`mktemp`
 kubeconfig=`mktemp`
+current_context=`kubectl config current-context`
 
-print Creating a new private key and certificate signing request
-openssl req -new -newkey $KEY_TYPE -keyout $key -nodes -out $csr -subj "/CN=${subject}" >&2
+for cluster_name in preprod-fss preprod-sbs prod-fss prod-sbs; do
 
-print Request certificate signing through a Kubernetes resource
-cat > $kubecsr <<EOF
+  kubectl config set current-context $cluster_name >&2
+
+  cluster_name=`kubectl config view --minify -o=jsonpath='{.clusters[0].name}'`
+  api_server_url=`kubectl config view --minify -o=jsonpath='{.clusters[0].cluster.server}'`
+  credentials_name="${cluster_name}-${subject}"
+
+  print Creating a TLS certificate in ${cluster_name} with CN="${subject}"
+  print Kubernetes API server is ${api_server_url}
+
+  key=`mktemp`
+  cert=`mktemp`
+  csr=`mktemp`
+  kubecsr=`mktemp`
+
+  print Creating a new private key and certificate signing request
+  openssl req -new -newkey $KEY_TYPE -keyout $key -nodes -out $csr -subj "/CN=${subject}" >&2
+
+  print Request certificate signing through a Kubernetes resource
+  cat > $kubecsr <<EOF
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
 metadata:
@@ -58,20 +65,24 @@ spec:
     - client auth
 EOF
 
-kubectl create -n default -f $kubecsr >&2
+  kubectl create -n default -f $kubecsr >&2
 
-print Signing the certificate using the Kubernetes CA
-kubectl certificate approve $subject -n default >&2
+  print Signing the certificate using the Kubernetes CA
+  kubectl certificate approve $subject -n default >&2
 
-print Downloading the signed certificate and delete it from Kubernetes
-kubectl get csr $subject -o jsonpath='{.status.certificate}' | base64 --decode > $cert
-kubectl delete csr $subject >&2
+  print Downloading the signed certificate and delete it from Kubernetes
+  kubectl get csr $subject -o jsonpath='{.status.certificate}' | base64 --decode > $cert
+  kubectl delete csr $subject >&2
 
-export KUBECONFIG=$kubeconfig
-kubectl config set-cluster $cluster_name --server $api_server_url --insecure-skip-tls-verify >&2
-kubectl config set-credentials $subject --client-key $key --client-certificate $cert --embed-certs >&2
-kubectl config set-context $cluster_name --cluster $cluster_name --user $subject --namespace default >&2
-kubectl config set current-context $cluster_name >&2
+  kubectl --kubeconfig $kubeconfig config set-cluster $cluster_name --server $api_server_url --insecure-skip-tls-verify >&2
+  kubectl --kubeconfig $kubeconfig config set-credentials $credentials_name --client-key $key --client-certificate $cert --embed-certs >&2
+  kubectl --kubeconfig $kubeconfig config set-context $cluster_name --cluster $cluster_name --user $credentials_name --namespace default >&2
+  kubectl --kubeconfig $kubeconfig config set current-context $cluster_name >&2
+
+  rm -f $key $cert $csr $kubecsr
+done
+
+kubectl config set-context $current_context >&2
 
 print
 print Your kubeconfig is ready:
@@ -79,5 +90,3 @@ print
 echo '# vi: se ft=yaml:'
 echo '---'
 cat $kubeconfig
-
-rm -f $key $cert $csr $kubecsr $kubeconfig
