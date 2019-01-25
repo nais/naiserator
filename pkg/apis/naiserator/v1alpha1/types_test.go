31201fc2df2fe5436d6d67f9ba47cbc0f29487f6
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-01-25 08:10:52 +0100

    Sjekker default-verdi, ikke nil

--- pkg/apis/naiserator/v1alpha1/types_test.go
+++ pkg/apis/naiserator/v1alpha1/types_test.go
@@ -26,5 +26,5 @@ func TestNilFix(t *testing.T) {
 	app.NilFix()
 	assert.NotNil(t, app.Spec.Ingresses)
 	assert.NotNil(t, app.Spec.Env)
-	assert.NotNil(t, app.Spec.Service.Port)
+	assert.Equal(t, int32(v1alpha1.DefaultPort), app.Spec.Service.Port)
 }
