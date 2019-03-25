a39f1a62662fba8a3a4b3a7ff206e890e2ff1af6
Line Moseng <line.moseng@nav.no>
2019-03-25 16:26:09 +0100

    yrdyinh

--- pkg/resourcecreator/networkpolicy_test.go
+++ pkg/resourcecreator/networkpolicy_test.go
@@ -8,21 +8,19 @@ import (
 )
 
 func TestNetworkPolicy(t *testing.T) {
-	t.Run("Test that allow all removes all policy types", func(t *testing.T) {
+	t.Run("Test that allow all policy sets rules to empty object", func(t *testing.T) {
 		app := fixtures.Application()
 		app.Spec.AccessPolicy.Egress.AllowAll = true
-		app.Spec.AccessPolicy.Ingress.AllowAll = true
 		networkPolicy := NetworkPolicy(app)
 
-		assert.Empty(t, networkPolicy.Spec.PolicyTypes)
+		assert.Empty(t, networkPolicy.Spec.Egress)
 	})
 
-	t.Run("If allow all egress, only ingress policy type in NetworkPolicy", func(t *testing.T) {
+	t.Run("Test that default deny all sets rules to empty list", func(t *testing.T) {
 		app := fixtures.Application()
-		app.Spec.AccessPolicy.Egress.Rules = append(app.Spec.AccessPolicy.Egress.Rules, "example.com")
 		networkPolicy := NetworkPolicy(app)
 
-		assert.Len(t, networkPolicy.Spec.PolicyTypes, 1)
-		assert.Equal(t, networking.PolicyTypeIngress, networkPolicy.Spec.PolicyTypes[0])
+		assert.Equal(t, []networking.NetworkPolicyEgressRule{}, networkPolicy.Spec.Egress)
 	})
+
 }
