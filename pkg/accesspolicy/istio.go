1c9e643ec1c57941507d8f67349d0163c4ff6410
Line Moseng <line.moseng@nav.no>
2019-05-07 09:53:52 +0200

    Istio istio istio

--- pkg/accesspolicy/istio.go
+++ pkg/accesspolicy/istio.go
@@ -1,6 +1,7 @@
 package accesspolicy
 
 import (
+	"fmt"
 	nais "github.com/nais/naiserator/pkg/apis/naiserator/v1alpha1"
 	istio_crd "github.com/nais/naiserator/pkg/apis/istio/v1alpha1"
 	k8s_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,22 +10,36 @@ import (
 
 const IstioAPIVersion = "v1alpha1"
 
-func getDefaultServiceRole() istio_crd.ServiceRoleSpec {
+
+func getServicePath(rule nais.AccessPolicyGressRule) string {
+	return fmt.Sprintf("%s.%s.svc.cluster.local", rule.Application, rule.Namespace)
+}
+
+func getAccessRules(rules []nais.AccessPolicyGressRule) (accessRules []*istio_crd.AccessRule) {
+	for _, gress := range rules {
+		policy := istio_crd.AccessRule{
+			Services: []string{getServicePath(gress)},
+			Methods: []string{"*"},
+		}
+
+		accessRules = append(accessRules, &policy)
+	}
+
+	return
+}
+
+
+func getServiceRole(app *nais.Application) istio_crd.ServiceRoleSpec {
 	return istio_crd.ServiceRoleSpec{
-		Rules: []*istio_crd.AccessRule{
-			{
-				Services: []string{""},
-				Methods:  []string{"*"},
-			},
-		},
+		Rules: getAccessRules(app.Spec.AccessPolicy.Ingress.Rules),
 	}
 }
 
 func getDefaultServiceRoleBinding(appName string) istio_crd.ServiceRoleBindingSpec {
 	return istio_crd.ServiceRoleBindingSpec{
-		Subjects: []*istio_crd.Subject{{User: ""}},
+		Subjects: []*istio_crd.Subject{{User: "*"}},
 		RoleRef: &istio_crd.RoleRef{
-			Kind: "RoleRef",
+			Kind: "ServiceRole",
 			Name: appName,
 		},
 	}
@@ -38,7 +53,7 @@ func Istio(app *nais.Application) []runtime.Object {
 				APIVersion: IstioAPIVersion,
 			},
 			ObjectMeta: app.CreateObjectMeta(),
-			Spec: getDefaultServiceRole(),
+			Spec:       getServiceRole(app),
 		}, &istio_crd.ServiceRoleBinding{
 			TypeMeta: k8s_meta.TypeMeta{
 				Kind:       "ServiceRoleBinding",
