975c80bfe07aa53ca284268d7a503294034ee431
Johnny Horvi <johnny@horvi.no>
2019-05-15 07:31:06 +0200

    read access policy toggle default value from environment variable

--- cmd/naiserator/main.go
+++ cmd/naiserator/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -29,7 +30,7 @@ var (
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to Kubernetes config file")
 	flag.StringVar(&bindAddr, "bind-address", ":8080", "ip:port where http requests are served")
-	flag.BoolVar(&accessPolicyEnabled, "access-policy-enabled", false, "enable access policy with Istio and NetworkPolicies")
+	flag.BoolVar(&accessPolicyEnabled, "access-policy-enabled", ensureBool(getEnv("ACCESS_POLICY_ENABLED", "false")), "enable access policy with Istio and NetworkPolicies")
 	flag.Parse()
 }
 
@@ -122,4 +123,21 @@ func StopCh() (stopCh <-chan struct{}) {
 	}()
 
 	return stop
+}
+
+func ensureBool(str string) bool {
+	bool, err := strconv.ParseBool(str)
+
+	if err != nil {
+		log.Errorf("unable to parse boolean \"%s\", defaulting to false", str)
+	}
+
+	return bool
+}
+
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
