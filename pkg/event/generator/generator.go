db9f52c18ad42218d5d1ff5595e7ce2fb2bd89d8
Kim Tore Jensen <kimtjen@gmail.com>
2019-07-15 14:49:11 +0200

    move code to deployment event generation module

---
+++ pkg/event/generator/generator.go
@@ -0,0 +1,53 @@
+package generator
+
+import (
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
+	"github.com/nais/naiserator/pkg/event"
+)
+
+func NewDeploymentEvent(app nais.Application) deployment.Event {
+	image := containerImage(app.Spec.Image)
+
+	return deployment.Event{
+		Application:   app.Name,
+		Cluster:       app.ClusterName,
+		CorrelationID: uuid.New().String(),
+		Environment:   environment(app),
+		Image:         &image,
+		Namespace:     app.Namespace,
+		RolloutStatus: deployment.RolloutStatus_initialized,
+		Source:        deployment.System_naiserator,
+		Team:          app.Labels["team"],
+		Timestamp:     time.Now().Unix(),
+		Version:       image.Tag,
+		Platform: &deployment.Platform{
+			Type:    deployment.PlatformType_nais,
+			Variant: "naiserator",
+		},
+	}
+}
+
+func environment(app nais.Application) deployment.Environment {
+	if strings.HasPrefix(app.ClusterName, "prod-") {
+		return deployment.Environment_production
+	}
+	return deployment.Environment_development
+}
+
+func containerImage(imageName string) deployment.ContainerImage {
+	parts := strings.SplitN(imageName, ":", 2)
+	tag := "latest"
+	if len(parts) != 1 {
+		tag = parts[1]
+	}
+	return deployment.ContainerImage{
+		Name: parts[0],
+		Tag:  tag,
+		Hash: "TODO",
+	}
+}
+
