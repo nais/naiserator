1b46665b771e85948d5e13aa453be3f94570aa10
Line Moseng <line.moseng@nav.no>
2019-09-27 11:54:39 +0200

    Removed new line

--- pkg/resourcecreator/networkpolicy.go
+++ pkg/resourcecreator/networkpolicy.go
@@ -6,7 +6,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-
 func networkPolicyRules(rules []nais.AccessPolicyGressRule) (networkPolicy []networkingv1.NetworkPolicyPeer) {
 	for _, rule := range rules {
 		networkPolicyPeer := networkingv1.NetworkPolicyPeer{
