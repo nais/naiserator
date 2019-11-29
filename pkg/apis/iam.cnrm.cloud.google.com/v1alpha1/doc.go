044f275a58cf8564d47f94f02e366f18d7750ac0
Frode Sundby <frodesun@gmail.com>
2019-11-29 10:47:58 +0100

    Adding google IAM CRD spec

---
+++ pkg/apis/iam.cnrm.cloud.google.com/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+
+// Package v1alpha1 is the v1alpha1 version of the API.
+// +groupName=iam.cnrm.cloud.google.com
+package v1alpha1
---
+++ pkg/apis/iam.cnrm.cloud.google.com/v1alpha1/types.go
@@ -0,0 +1,26 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type IAMServiceAccount struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec IAMServiceAccountSpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type IAMServiceAccountList struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Items []IAMServiceAccount `json:"items"`
+}
+
+type IAMServiceAccountSpec struct {
+	DisplayName string `json:"displayName"`
+}
