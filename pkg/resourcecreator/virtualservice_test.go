cfbe18e8c2347f679e918f25575722870079f3fd
Kim Tore Jensen <kimtjen@gmail.com>
2019-12-10 11:57:26 +0100

    Remove domain HTTPS mangling when using Istio
    This patch removes special handling logic of VirtualService URLs,
    replaced by standard url.Parse().
    
    Additionally, fixes a nil crash when an ingress is composed of just a
    hostname without the TLD component.

--- pkg/resourcecreator/virtualservice_test.go
+++ pkg/resourcecreator/virtualservice_test.go
@@ -2,9 +2,10 @@ package resourcecreator_test
 
 import (
 	"fmt"
-	"github.com/nais/naiserator/pkg/resourcecreator"
 	"strings"
 	"testing"
+
+	"github.com/nais/naiserator/pkg/resourcecreator"
 
 	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
 	"github.com/nais/naiserator/pkg/test/fixtures"
@@ -12,11 +13,11 @@ import (
 )
 
 func TestVirtualService(t *testing.T) {
-	t.Run("virtualservice created according to spec", func(t *testing.T) {
+	t.Run("virtualservices have correct gateways", func(t *testing.T) {
 		ingresses := []string{
-			"first.host.no",
+			"https://host.no",
 			"https://second.host.no",
-			"http://another.domain.tk",
+			"https://subdomain.third.host.no",
 		}
 
 		app := fixtures.MinimalApplication()
@@ -24,8 +25,56 @@ func TestVirtualService(t *testing.T) {
 		err := nais.ApplyDefaults(app)
 		assert.NoError(t, err)
 
-		vses,err  := resourcecreator.VirtualServices(app)
+		vses, err := resourcecreator.VirtualServices(app)
 		assert.Len(t, vses, 3)
+
+		for i := range vses {
+			assert.Equal(t, fmt.Sprintf(resourcecreator.IstioGatewayPrefix, "host-no"), vses[i].Spec.Gateways[0])
+		}
+	})
+
+	t.Run("virtualservice gateway copes with missing TLD", func(t *testing.T) {
+		ingresses := []string{
+			"https://foo",
+		}
+
+		app := fixtures.MinimalApplication()
+		app.Spec.Ingresses = ingresses
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		vses, err := resourcecreator.VirtualServices(app)
+		assert.Len(t, vses, 1)
+		assert.Equal(t, fmt.Sprintf(resourcecreator.IstioGatewayPrefix, "foo"), vses[0].Spec.Gateways[0])
+	})
+
+	t.Run("virtualservices not created on invalid ingress", func(t *testing.T) {
+		ingresses := []string{
+			"host.no",
+		}
+
+		app := fixtures.MinimalApplication()
+		app.Spec.Ingresses = ingresses
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		vses, err := resourcecreator.VirtualServices(app)
+		assert.Error(t, err)
+		assert.Nil(t, vses)
+	})
+
+	t.Run("virtualservice created according to spec", func(t *testing.T) {
+		ingresses := []string{
+			"https://first.host.no",
+		}
+
+		app := fixtures.MinimalApplication()
+		app.Spec.Ingresses = ingresses
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		vses, err := resourcecreator.VirtualServices(app)
+		assert.Len(t, vses, 1)
 
 		assert.Equal(t, fmt.Sprintf(resourcecreator.IstioGatewayPrefix, "host-no"), vses[0].Spec.Gateways[0])
 		assert.Len(t, vses[0].Spec.HTTP, 1)
@@ -34,7 +83,7 @@ func TestVirtualService(t *testing.T) {
 		assert.Equal(t, app.Name, route.Destination.Host)
 		assert.Equal(t, uint32(app.Spec.Service.Port), route.Destination.Port.Number)
 		assert.Equal(t, resourcecreator.IstioVirtualServiceTotalWeight, route.Weight)
-		assert.True(t, strings.HasPrefix(vses[0].Name, app.Name + "-first-host-no"))
+		assert.True(t, strings.HasPrefix(vses[0].Name, app.Name+"-first-host-no"))
 		assert.Equal(t, app.Namespace, vses[0].Namespace)
 	})
 }
