f1c9ef78d39c835f1855859c3031de099f600836
rbjornstad <roger.bjornstad@nav.no>
2020-01-20 16:01:19 +0100

    Fixing bucket name

--- pkg/resourcecreator/googlestoragebucket.go
+++ pkg/resourcecreator/googlestoragebucket.go
@@ -2,15 +2,15 @@ package resourcecreator
 
 import (
 	"fmt"
+
 	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
 	google_storage_crd "github.com/nais/naiserator/pkg/apis/storage.cnrm.cloud.google.com/v1beta1"
-	"github.com/nais/naiserator/pkg/util"
 	k8s_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func GoogleStorageBucket(app *nais.Application, bucket nais.CloudStorageBucket) *google_storage_crd.StorageBucket {
 	objectMeta := app.CreateObjectMeta()
-	objectMeta.Name = fmt.Sprintf("%s-%s", bucket.NamePrefix, util.RandomString(6))
+	objectMeta.Name = fmt.Sprintf("%s.%s", bucket.NamePrefix, app.CreateAppNamespaceHash())
 
 	if !bucket.CascadingDelete {
 		ApplyAbandonDeletionPolicy(&objectMeta)
---
+++ pkg/resourcecreator/googlestoragebucket_test.go
@@ -0,0 +1,25 @@
+package resourcecreator_test
+
+import (
+"testing"
+
+"github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
+"github.com/nais/naiserator/pkg/resourcecreator"
+"github.com/nais/naiserator/pkg/test/fixtures"
+"github.com/stretchr/testify/assert"
+)
+
+func TestGetGoogleStorageBucket(t *testing.T) {
+	t.Run("bucket creation", func(t *testing.T) {
+		bucketname := "buckowens"
+		app := fixtures.MinimalApplication()
+		app.Spec.GCP = &v1alpha1.GCP{
+			Buckets: []v1alpha1.CloudStorageBucket{
+				{NamePrefix: bucketname},
+			},
+		}
+		bucket := resourcecreator.GoogleStorageBucket(app, app.Spec.GCP.Buckets[0])
+		assert.Equal(t, "buckowens.myapplicati-mynamespac-w4o5cwa", bucket.Name)
+		assert.Equal(t, resourcecreator.GoogleRegion, bucket.Spec.Location)
+	})
+}
