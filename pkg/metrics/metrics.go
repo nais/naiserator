57341d3495048868637b10c10114e3241707d77d
Kim Tore Jensen <kimtjen@gmail.com>
2020-01-23 11:27:03 +0100

    use linear buckets for kubernetes_resource_write_duration metric, to better support grafana

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -49,7 +49,7 @@ var (
 		Name:      "kubernetes_resource_write_duration",
 		Namespace: "naiserator",
 		Help:      "request duration when talking to Kubernetes",
-		Buckets:   []float64{0.001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60},
+		Buckets:   prometheus.LinearBuckets(0.01, 0.01, 100),
 	})
 )
 
