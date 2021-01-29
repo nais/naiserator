91bf216e5cf5aeb92093c8f82ac246393a1aaab0
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-01-29 14:39:13 +0100

    Create PodDisruptionBudget for relevant apps
    relevant meaning max != 1 and min != max replicas

---
+++ pkg/resourcecreator/poddisruptionbudget.go
@@ -0,0 +1,37 @@
+package resourcecreator
+
+import (
+	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
+	"k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func PodDisruptionBudget(app *nais.Application) *v1beta1.PodDisruptionBudget {
+	if app.Spec.Replicas.Max == 1 {
+		return nil
+	}
+
+	if app.Spec.Replicas.Min == app.Spec.Replicas.Max {
+		return nil
+	}
+
+	return &v1beta1.PodDisruptionBudget{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "PodDisruptionPudget",
+			APIVersion: "policy/v1beta1",
+		},
+		ObjectMeta: app.CreateObjectMeta(),
+		Spec: v1beta1.PodDisruptionBudgetSpec{
+			MinAvailable: &intstr.IntOrString{
+				Type:   intstr.Int,
+				IntVal: 1,
+			},
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": app.Name,
+				},
+			},
+		},
+	}
+}
---
+++ pkg/resourcecreator/poddisruptionbudget_test.go
@@ -0,0 +1,45 @@
+package resourcecreator_test
+
+import (
+	"testing"
+
+	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
+	"github.com/nais/naiserator/pkg/resourcecreator"
+	"github.com/nais/naiserator/pkg/test/fixtures"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPodDisruptionBudget(t *testing.T) {
+	t.Run("normal app should not have pdb", func(t *testing.T) {
+		app := fixtures.MinimalApplication()
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		pdb := resourcecreator.PodDisruptionBudget(app)
+
+		assert.Equal(t, app.Name, pdb.Name)
+		assert.Equal(t, app.Namespace, pdb.Namespace)
+		assert.Equal(t, int32(1), pdb.Spec.MinAvailable.IntVal)
+		assert.Equal(t, app.Name, pdb.Spec.Selector.MatchLabels["app"])
+	})
+
+	t.Run("min = max replicas should not have pdb", func(t *testing.T) {
+		app := fixtures.MinimalApplication()
+		app.Spec.Replicas.Min = app.Spec.Replicas.Max
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		pdb := resourcecreator.PodDisruptionBudget(app)
+		assert.Nil(t, pdb)
+	})
+
+	t.Run("max replicas = 1 should not have pdb", func(t *testing.T) {
+		app := fixtures.MinimalApplication()
+		app.Spec.Replicas.Max = 1
+		err := nais.ApplyDefaults(app)
+		assert.NoError(t, err)
+
+		pdb := resourcecreator.PodDisruptionBudget(app)
+		assert.Nil(t, pdb)
+	})
+}
