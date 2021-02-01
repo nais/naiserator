1afd6ef049ca5cd158ec7a699d3c4b4ed11a0932
Kim Tore Jensen <kimtjen@gmail.com>
2021-02-01 15:17:30 +0100

    improve read-only client log messages

--- pkg/readonly/client.go
+++ pkg/readonly/client.go
@@ -23,35 +23,37 @@ type Client struct {
 }
 
 func (c *Client) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+	// log.Debugf("Read-only client: GET %s", naiserator_scheme.TypeName(obj))
 	return c.client.Get(ctx, key, obj)
 }
 
 func (c *Client) List(ctx context.Context, list runtime.Object, opts ...client.ListOption) error {
+	// log.Debugf("Read-only client: LIST %s", naiserator_scheme.TypeName(list))
 	return c.client.List(ctx, list, opts...)
 }
 
 func (c *Client) Create(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
-	log.Debugf("Read-only client: CREATE %s", naiserator_scheme.TypeName(obj))
+	log.Debugf("Read-only client ignoring CREATE %s", naiserator_scheme.TypeName(obj))
 	return nil
 }
 
 func (c *Client) Delete(ctx context.Context, obj runtime.Object, opts ...client.DeleteOption) error {
-	log.Debugf("Read-only client: DELETE %s", naiserator_scheme.TypeName(obj))
+	log.Debugf("Read-only client ignoring DELETE %s", naiserator_scheme.TypeName(obj))
 	return nil
 }
 
 func (c *Client) Update(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
-	log.Debugf("Read-only client: UPDATE %s", naiserator_scheme.TypeName(obj))
+	log.Debugf("Read-only client ignoring UPDATE %s", naiserator_scheme.TypeName(obj))
 	return nil
 }
 
 func (c *Client) Patch(ctx context.Context, obj runtime.Object, patch client.Patch, opts ...client.PatchOption) error {
-	log.Debugf("Read-only client: PATCH %s", naiserator_scheme.TypeName(obj))
+	log.Debugf("Read-only client ignoring PATCH %s", naiserator_scheme.TypeName(obj))
 	return nil
 }
 
 func (c *Client) DeleteAllOf(ctx context.Context, obj runtime.Object, opts ...client.DeleteAllOfOption) error {
-	log.Debugf("Read-only client: DELETE ALL OF %s", naiserator_scheme.TypeName(obj))
+	log.Debugf("Read-only client ignoring DELETE ALL OF %s", naiserator_scheme.TypeName(obj))
 	return nil
 }
 
