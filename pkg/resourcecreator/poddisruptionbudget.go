6bd5aa25b5ece7c510d49025d1b1fb3adbdbbb92
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-02-05 01:45:36 +0100

    rebase on master

--- pkg/resourcecreator/poddisruptionbudget.go
+++ pkg/resourcecreator/poddisruptionbudget.go
@@ -1,13 +1,13 @@
 package resourcecreator
 
 import (
-	nais "github.com/nais/naiserator/pkg/apis/nais.io/v1alpha1"
-	"k8s.io/api/policy/v1beta1"
+	nais_io_v1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func PodDisruptionBudget(app *nais.Application) *v1beta1.PodDisruptionBudget {
+func PodDisruptionBudget(app *nais_io_v1alpha1.Application) *policyv1beta1.PodDisruptionBudget {
 	if app.Spec.Replicas.Max == 1 {
 		return nil
 	}
@@ -16,13 +16,13 @@ func PodDisruptionBudget(app *nais.Application) *v1beta1.PodDisruptionBudget {
 		return nil
 	}
 
-	return &v1beta1.PodDisruptionBudget{
+	return &policyv1beta1.PodDisruptionBudget{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "PodDisruptionPudget",
 			APIVersion: "policy/v1beta1",
 		},
 		ObjectMeta: app.CreateObjectMeta(),
-		Spec: v1beta1.PodDisruptionBudgetSpec{
+		Spec: policyv1beta1.PodDisruptionBudgetSpec{
 			MinAvailable: &intstr.IntOrString{
 				Type:   intstr.Int,
 				IntVal: 1,
