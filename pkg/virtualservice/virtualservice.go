b04154b3a56eaece3edbb73b1cb36d9e61ea5f63
Tommy Trøen <tommy.troen@nav.no>
2021-02-17 15:35:20 +0100

    wip: virtualservices

--- pkg/virtualservice/virtualservice.go
+++ pkg/virtualservice/virtualservice.go
@@ -1,15 +1,16 @@
 package virtualservice
 
 import (
-	"fmt"
-	"net/url"
-	"sort"
-	"strings"
+    "fmt"
+    "net/url"
+    "sort"
+    "strings"
 
-	"github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
-	networking_istio_io_v1alpha3 "github.com/nais/liberator/pkg/apis/networking.istio.io/v1alpha3"
-	"github.com/nais/naiserator/pkg/naiserator/config"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
+    networking_istio_io_v1alpha3 "github.com/nais/liberator/pkg/apis/networking.istio.io/v1alpha3"
+    v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+    "github.com/nais/naiserator/pkg/naiserator/config"
 )
 
 const IstioVirtualServiceTotalWeight int32 = 100 // The total weight of all routes must equal 100
@@ -19,146 +20,162 @@ const regexSuffix = "(/.*)?"
 type Gateway string
 
 type Route struct {
-	Gateway Gateway
-	Route   networking_istio_io_v1alpha3.HTTPRoute
+    Gateway Gateway
+    Route   networking_istio_io_v1alpha3.HTTPRoute
 }
 
 type RouteMap map[url.URL]networking_istio_io_v1alpha3.HTTPRoute
 
 type Registry struct {
-	routes   RouteMap
-	mappings []config.GatewayMapping
+    routes   RouteMap
+    mappings []config.GatewayMapping
+}
+
+func (r *Registry) VirtualServices(app *nais_io_v1alpha1.Application) []networking_istio_io_v1alpha3.VirtualService {
+    services := make([]networking_istio_io_v1alpha3.VirtualService, 0)
+    hostSet := make(map[string]interface{})
+    for _, ingress := range app.Spec.Ingresses {
+        ingressUrl, err := url.Parse(string(ingress))
+        if err != nil {
+            continue
+        }
+        hostSet[ingressUrl.Host] = new(interface{})
+    }
+    for host := range hostSet {
+        services = append(services, r.VirtualService(host))
+    }
+    return services
 }
 
 func New(gatewayMapping []config.GatewayMapping) *Registry {
-	return &Registry{
-		routes:   make(RouteMap),
-		mappings: gatewayMapping,
-	}
+    return &Registry{
+        routes:   make(RouteMap),
+        mappings: gatewayMapping,
+    }
 }
 
 func ResolveGateway(host string, mappings []config.GatewayMapping) []string {
-	for _, mapping := range mappings {
-		if strings.HasSuffix(host, mapping.DomainSuffix) {
-			return strings.Split(mapping.GatewayName, ",")
-		}
-	}
-	return nil
+    for _, mapping := range mappings {
+        if strings.HasSuffix(host, mapping.DomainSuffix) {
+            return strings.Split(mapping.GatewayName, ",")
+        }
+    }
+    return nil
 }
 
-func (v *Registry) Routes(host string) []networking_istio_io_v1alpha3.HTTPRoute {
-	routes := make([]networking_istio_io_v1alpha3.HTTPRoute, 0, len(v.routes))
-	for parsedURL, route := range v.routes {
-		if parsedURL.Host != host {
-			continue
-		}
-		routes = append(routes, route)
-	}
+func (r *Registry) Routes(host string) []networking_istio_io_v1alpha3.HTTPRoute {
+    routes := make([]networking_istio_io_v1alpha3.HTTPRoute, 0, len(r.routes))
+    for parsedURL, route := range r.routes {
+        if parsedURL.Host != host {
+            continue
+        }
+        routes = append(routes, route)
+    }
 
-	SortRoutes(routes)
+    SortRoutes(routes)
 
-	return routes
+    return routes
 }
 
-func (v *Registry) VirtualService(host string) networking_istio_io_v1alpha3.VirtualService {
-	return networking_istio_io_v1alpha3.VirtualService{
-		TypeMeta: v1.TypeMeta{
-			Kind:       "VirtualService",
-			APIVersion: networking_istio_io_v1alpha3.GroupVersion.Version,
-		},
-		ObjectMeta: v1.ObjectMeta{
-			Name:      host,
-			Namespace: "namespace",
-		},
-		Spec: networking_istio_io_v1alpha3.VirtualServiceSpec{
-			Gateways: ResolveGateway(host, v.mappings),
-			Hosts:    []string{host},
-			HTTP:     v.Routes(host),
-		},
-	}
+func (r *Registry) VirtualService(host string) networking_istio_io_v1alpha3.VirtualService {
+    return networking_istio_io_v1alpha3.VirtualService{
+        TypeMeta: v1.TypeMeta{
+            Kind:       "VirtualService",
+            APIVersion: networking_istio_io_v1alpha3.GroupVersion.Version,
+        },
+        ObjectMeta: v1.ObjectMeta{
+            Name:      host,
+            Namespace: "namespace",
+        },
+        Spec: networking_istio_io_v1alpha3.VirtualServiceSpec{
+            Gateways: ResolveGateway(host, r.mappings),
+            Hosts:    []string{host},
+            HTTP:     r.Routes(host),
+        },
+    }
 }
 
-func (v *Registry) Add(app *nais_io_v1alpha1.Application) error {
-	routes, err := httpRoutes(app)
-	if err != nil {
-		return err
-	}
+func (r *Registry) Add(app *nais_io_v1alpha1.Application) error {
+    routes, err := httpRoutes(app)
+    if err != nil {
+        return err
+    }
 
-	// fixme: collisions: check same app
+    // fixme: collisions: check same app
 
-	for parsedURL, route := range routes {
-		v.routes[parsedURL] = route
-	}
+    for parsedURL, route := range routes {
+        r.routes[parsedURL] = route
+    }
 
-	return nil
+    return nil
 }
 
 func httpRoutes(app *nais_io_v1alpha1.Application) (RouteMap, error) {
-	routes := make(RouteMap, 0)
-	for _, ingress := range app.Spec.Ingresses {
+    routes := make(RouteMap, 0)
+    for _, ingress := range app.Spec.Ingresses {
 
-		parsedUrl, err := url.Parse(string(ingress))
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse URL '%s': %s", ingress, err)
-		}
+        parsedUrl, err := url.Parse(string(ingress))
+        if err != nil {
+            return nil, fmt.Errorf("failed to parse URL '%s': %s", ingress, err)
+        }
 
-		if len(parsedUrl.Path) == 0 {
-			parsedUrl.Path = "/"
-		}
+        if len(parsedUrl.Path) == 0 {
+            parsedUrl.Path = "/"
+        }
 
-		/* FIXME
-		err = validateUrl(parsedUrl)
-		if err != nil {
-			return nil, err
-		}
+        /* FIXME
+           err = validateUrl(parsedUrl)
+           if err != nil {
+           	return nil, err
+           }
 
-		*/
+        */
 
-		route := httpRoute(parsedUrl.Path, app)
-		routes[*parsedUrl] = route
+        route := httpRoute(parsedUrl.Path, app)
+        routes[*parsedUrl] = route
 
-	}
-	return routes, nil
+    }
+    return routes, nil
 }
 
 func httpRoute(path string, app *nais_io_v1alpha1.Application) networking_istio_io_v1alpha3.HTTPRoute {
-	return networking_istio_io_v1alpha3.HTTPRoute{
-		Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
-			{
-				URI: networking_istio_io_v1alpha3.StringMatch{
-					Regex: path + regexSuffix,
-				},
-			},
-		},
-		Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
-			{
-				Destination: networking_istio_io_v1alpha3.Destination{
-					Host: app.Name,
-					Port: networking_istio_io_v1alpha3.PortSelector{
-						Number: uint32(app.Spec.Service.Port),
-					},
-				},
-				Weight: IstioVirtualServiceTotalWeight,
-			},
-		},
-	}
+    return networking_istio_io_v1alpha3.HTTPRoute{
+        Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
+            {
+                URI: networking_istio_io_v1alpha3.StringMatch{
+                    Regex: path + regexSuffix,
+                },
+            },
+        },
+        Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
+            {
+                Destination: networking_istio_io_v1alpha3.Destination{
+                    Host: app.Name,
+                    Port: networking_istio_io_v1alpha3.PortSelector{
+                        Number: uint32(app.Spec.Service.Port),
+                    },
+                },
+                Weight: IstioVirtualServiceTotalWeight,
+            },
+        },
+    }
 }
 
 func SortRoutes(routes []networking_istio_io_v1alpha3.HTTPRoute) {
-	// Sort by name first
-	sort.Slice(routes, func(a, b int) bool {
-		return routes[a].Match[0].URI.Regex < routes[b].Match[0].URI.Regex
-	})
+    // Sort by name first
+    sort.Slice(routes, func(a, b int) bool {
+        return routes[a].Match[0].URI.Regex < routes[b].Match[0].URI.Regex
+    })
 
-	// Sort by actual path length
-	sort.SliceStable(routes, func(a, b int) bool {
-		return len(routes[a].Match[0].URI.Regex) > len(routes[b].Match[0].URI.Regex)
-	})
+    // Sort by actual path length
+    sort.SliceStable(routes, func(a, b int) bool {
+        return len(routes[a].Match[0].URI.Regex) > len(routes[b].Match[0].URI.Regex)
+    })
 
-	// Sort by URL path length
-	sort.SliceStable(routes, func(a, b int) bool {
-		segA := strings.Split(routes[a].Match[0].URI.Regex, "/")
-		segB := strings.Split(routes[b].Match[0].URI.Regex, "/")
-		return len(segA) > len(segB)
-	})
+    // Sort by URL path length
+    sort.SliceStable(routes, func(a, b int) bool {
+        segA := strings.Split(routes[a].Match[0].URI.Regex, "/")
+        segB := strings.Split(routes[b].Match[0].URI.Regex, "/")
+        return len(segA) > len(segB)
+    })
 }
--- pkg/virtualservice/virtualservice_test.go
+++ pkg/virtualservice/virtualservice_test.go
@@ -1,148 +1,186 @@
 package virtualservice_test
 
 import (
-	nais_io_v1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
-	"github.com/nais/naiserator/pkg/naiserator/config"
-	"github.com/nais/naiserator/pkg/test/fixtures"
-	"github.com/stretchr/testify/assert"
-	"testing"
+    "sort"
+    "testing"
 
-	networking_istio_io_v1alpha3 "github.com/nais/liberator/pkg/apis/networking.istio.io/v1alpha3"
-	"github.com/nais/naiserator/pkg/virtualservice"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+    nais_io_v1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
+    "github.com/stretchr/testify/assert"
+
+    "github.com/nais/naiserator/pkg/naiserator/config"
+    "github.com/nais/naiserator/pkg/test/fixtures"
+
+    networking_istio_io_v1alpha3 "github.com/nais/liberator/pkg/apis/networking.istio.io/v1alpha3"
+    "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+    "github.com/nais/naiserator/pkg/virtualservice"
 )
 
 var dst = networking_istio_io_v1alpha3.VirtualService{
-	TypeMeta:   v1.TypeMeta{},
-	ObjectMeta: v1.ObjectMeta{},
-	Spec: networking_istio_io_v1alpha3.VirtualServiceSpec{
-		Gateways: nil,
-	},
+    TypeMeta:   v1.TypeMeta{},
+    ObjectMeta: v1.ObjectMeta{},
+    Spec: networking_istio_io_v1alpha3.VirtualServiceSpec{
+        Gateways: nil,
+    },
 }
 
 func simpleRoute(uri string) networking_istio_io_v1alpha3.HTTPRoute {
-	return networking_istio_io_v1alpha3.HTTPRoute{
-		Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
-			{
-				URI: networking_istio_io_v1alpha3.StringMatch{
-					Regex: uri,
-				},
-			},
-		},
-		Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
-			{
-				Destination: networking_istio_io_v1alpha3.Destination{
-					Host: "simplehost",
-					Port: networking_istio_io_v1alpha3.PortSelector{
-						Number: 80,
-					},
-				},
-				Weight: 100,
-			},
-		},
-	}
+    return networking_istio_io_v1alpha3.HTTPRoute{
+        Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
+            {
+                URI: networking_istio_io_v1alpha3.StringMatch{
+                    Regex: uri,
+                },
+            },
+        },
+        Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
+            {
+                Destination: networking_istio_io_v1alpha3.Destination{
+                    Host: "simplehost",
+                    Port: networking_istio_io_v1alpha3.PortSelector{
+                        Number: 80,
+                    },
+                },
+                Weight: 100,
+            },
+        },
+    }
+}
+
+func appWithIngress(ingress string) *nais_io_v1alpha1.Application {
+    app := fixtures.MinimalApplication()
+    app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
+        nais_io_v1alpha1.Ingress(ingress),
+    }
+    return app
+}
+
+func TestVirtualServices(t *testing.T) {
+    gatewayMappings := []config.GatewayMapping{
+        {
+            DomainSuffix: ".nav.no",
+            GatewayName:  "istio-system/gw-nav-no",
+        },
+    }
+    registry := virtualservice.New(gatewayMappings)
+    app := fixtures.MinimalApplication()
+    app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
+        "https://www.nav.no/some",
+        "https://app1.nav.no/some",
+        "https://www.nav2.no/some",
+    }
+    services := registry.VirtualServices(app)
+    hosts := []string{}
+    for _, vs := range services {
+        hosts = append(hosts, vs.Spec.Hosts...)
+    }
+
+    expectedHosts := []string{"www.nav.no", "app1.nav.no", "www.nav2.no"}
+    sort.Strings(expectedHosts)
+    sort.Strings(hosts)
+    assert.Equal(t, expectedHosts, hosts)
 }
 
 func TestSortRoutes(t *testing.T) {
-	routes := []networking_istio_io_v1alpha3.HTTPRoute{
-		simpleRoute("/base"),
-		simpleRoute("/base/some"),
-		simpleRoute("/base/some/thing"),
-		simpleRoute("/base/something"),
-		simpleRoute("/base/verylongstring"),
-		simpleRoute("/base/other"),
-		simpleRoute("/base/zzz"),
-		simpleRoute("/base/def"),
-		simpleRoute("/base/abc"),
-		simpleRoute("/zzzzzzz/many/path/segments/foo"),
-	}
+    routes := []networking_istio_io_v1alpha3.HTTPRoute{
+        simpleRoute("/base"),
+        simpleRoute("/base/some"),
+        simpleRoute("/base/some/thing"),
+        simpleRoute("/base/something"),
+        simpleRoute("/base/verylongstring"),
+        simpleRoute("/base/other"),
+        simpleRoute("/base/zzz"),
+        simpleRoute("/base/def"),
+        simpleRoute("/base/abc"),
+        simpleRoute("/zzzzzzz/many/path/segments/foo"),
+    }
 
-	correctOrder := []string{
-		"/zzzzzzz/many/path/segments/foo",
-		"/base/some/thing",
-		"/base/verylongstring",
-		"/base/something",
-		"/base/other",
-		"/base/some",
-		"/base/abc",
-		"/base/def",
-		"/base/zzz",
-		"/base",
-	}
+    correctOrder := []string{
+        "/zzzzzzz/many/path/segments/foo",
+        "/base/some/thing",
+        "/base/verylongstring",
+        "/base/something",
+        "/base/other",
+        "/base/some",
+        "/base/abc",
+        "/base/def",
+        "/base/zzz",
+        "/base",
+    }
 
-	if len(routes) != len(correctOrder) {
-		panic("pebkac: number of routes must match number of strings in correct order")
-	}
+    if len(routes) != len(correctOrder) {
+        panic("pebkac: number of routes must match number of strings in correct order")
+    }
 
-	virtualservice.SortRoutes(routes)
+    virtualservice.SortRoutes(routes)
 
-	for i := range routes {
-		assert.Equal(t, correctOrder[i], routes[i].Match[0].URI.Regex)
-	}
+    for i := range routes {
+        assert.Equal(t, correctOrder[i], routes[i].Match[0].URI.Regex)
+    }
 }
 
 func TestVirtualService(t *testing.T) {
-	app := fixtures.MinimalApplication()
-	app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
-		"https://www.nav.no/base/some/thing",
-	}
-	gatewayMappings := []config.GatewayMapping{
-		{
-			DomainSuffix: ".nav.no",
-			GatewayName:  "istio-system/gw-nav-no",
-		},
-	}
-	registry := virtualservice.New(gatewayMappings)
+    app := fixtures.MinimalApplication()
+    app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
+        "https://www.nav.no/base/some/thing",
+    }
+    gatewayMappings := []config.GatewayMapping{
+        {
+            DomainSuffix: ".nav.no",
+            GatewayName:  "istio-system/gw-nav-no",
+        },
+    }
+    registry := virtualservice.New(gatewayMappings)
 
-	err := registry.Add(app)
-	assert.NoError(t, err)
+    err := registry.Add(app)
+    assert.NoError(t, err)
 
-	vs := registry.VirtualService("www.nav.no")
+    vs := registry.VirtualService("www.nav.no")
 
-	assert.Equal(t, []string{"istio-system/gw-nav-no"}, vs.Spec.Gateways)
+    assert.Equal(t, []string{"istio-system/gw-nav-no"}, vs.Spec.Gateways)
 }
 
 func TestRoutes(t *testing.T) {
-	app := fixtures.MinimalApplication()
-	app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
-		"https://www.nav.no/base/some/thing",
-	}
+    app := fixtures.MinimalApplication()
+    app.Spec.Ingresses = []nais_io_v1alpha1.Ingress{
+        "https://www.nav.no/base/some/thing",
+    }
 
-	gatewayMappings := []config.GatewayMapping{
-		{
-			DomainSuffix: ".nav.no",
-			GatewayName:  "istio-system/gw-nav-no",
-		},
-	}
-	registry := virtualservice.New(gatewayMappings)
+    gatewayMappings := []config.GatewayMapping{
+        {
+            DomainSuffix: ".nav.no",
+            GatewayName:  "istio-system/gw-nav-no",
+        },
+    }
+    registry := virtualservice.New(gatewayMappings)
 
-	err := registry.Add(app)
-	assert.NoError(t, err)
+    err := registry.Add(app)
+    assert.NoError(t, err)
 
-	routes := registry.Routes("www.nav.no")
+    routes := registry.Routes("www.nav.no")
 
-	expectedRoutes := []networking_istio_io_v1alpha3.HTTPRoute{
-		{
-			Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
-				{
-					URI: networking_istio_io_v1alpha3.StringMatch{
-						Regex: "/base/some/thing(/.*)?",
-					},
-				},
-			},
-			Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
-				{
-					Destination: networking_istio_io_v1alpha3.Destination{
-						Host: app.Name,
-						Port: networking_istio_io_v1alpha3.PortSelector{
-							Number: uint32(app.Spec.Service.Port),
-						},
-					},
-					Weight: virtualservice.IstioVirtualServiceTotalWeight,
-				},
-			},
-		},
-	}
+    expectedRoutes := []networking_istio_io_v1alpha3.HTTPRoute{
+        {
+            Match: []networking_istio_io_v1alpha3.HTTPMatchRequest{
+                {
+                    URI: networking_istio_io_v1alpha3.StringMatch{
+                        Regex: "/base/some/thing(/.*)?",
+                    },
+                },
+            },
+            Route: []networking_istio_io_v1alpha3.HTTPRouteDestination{
+                {
+                    Destination: networking_istio_io_v1alpha3.Destination{
+                        Host: app.Name,
+                        Port: networking_istio_io_v1alpha3.PortSelector{
+                            Number: uint32(app.Spec.Service.Port),
+                        },
+                    },
+                    Weight: virtualservice.IstioVirtualServiceTotalWeight,
+                },
+            },
+        },
+    }
 
-	assert.Equal(t, expectedRoutes, routes)
+    assert.Equal(t, expectedRoutes, routes)
 }
