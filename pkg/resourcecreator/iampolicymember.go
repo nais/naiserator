8d872c2cab95a1b34a581bef0fafa481e2163089
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-03-15 09:08:33 +0100

    refactor: replace shortname util with namegen package in liberator

--- pkg/resourcecreator/iampolicymember.go
+++ pkg/resourcecreator/iampolicymember.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mitchellh/hashstructure"
 	google_iam_crd "github.com/nais/liberator/pkg/apis/iam.cnrm.cloud.google.com/v1beta1"
 	nais "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
-	"github.com/nais/naiserator/pkg/util"
+	"github.com/nais/liberator/pkg/namegen"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -47,7 +47,7 @@ func createIAMPolicyMemberName(app *nais.Application, policy nais.CloudIAMPermis
 		return "", fmt.Errorf("while calculating hash from policy: %w", err)
 	}
 	basename := fmt.Sprintf("%s-%s-%x", app.Name, strings.ToLower(policy.Resource.Kind), hash)
-	return util.StrShortName(basename, maxLengthResourceName)
+	return namegen.ShortName(basename, maxLengthResourceName)
 }
 
 func formatIAMExternalName(googleTeamProjectId, resourceName string) string {
--- pkg/resourcecreator/kafka.go
+++ pkg/resourcecreator/kafka.go
@@ -2,16 +2,14 @@ package resourcecreator
 
 import (
 	"fmt"
-	nais "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
-	"github.com/nais/naiserator/pkg/util"
-)
 
-const (
-	maxSecretNameLength = 63
+	nais "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
+	"github.com/nais/liberator/pkg/namegen"
 )
 
 func generateKafkaSecretName(app *nais.Application) (string, error) {
-	secretName, err := util.StrShortName(fmt.Sprintf("kafka-%s-%s", app.Name, app.Spec.Kafka.Pool), maxSecretNameLength)
+	secretName, err := namegen.ShortName(fmt.Sprintf("kafka-%s-%s", app.Name, app.Spec.Kafka.Pool), MaxSecretNameLength)
+
 	if err != nil {
 		return "", fmt.Errorf("unable to generate kafka secret name: %s", err)
 	}
