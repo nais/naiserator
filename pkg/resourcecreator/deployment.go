b0abebb6b7f936c722b0778f45bad759d885dfe2
Kyrre Havik <kyrremann@gmail.com>
2021-04-08 10:03:20 +0200

    Revert "F: ny hemmelighet for Kafkarator"
    This reverts commit 48afa6cff483244985b7749b676a90841bdb495c.

--- pkg/resourcecreator/deployment.go
+++ pkg/resourcecreator/deployment.go
@@ -35,7 +35,6 @@ const (
 	kafkaSchemaRegistryUserKey     = "KAFKA_SCHEMA_REGISTRY_USER"
 	kafkaSchemaRegistryPasswordKey = "KAFKA_SCHEMA_REGISTRY_PASSWORD"
 	kafkaCredStorePasswordKey      = "KAFKA_CREDSTORE_PASSWORD"
-	KafkaSecretUpdated             = "KAFKA_SECRET_UPDATED"
 	kafkaCertificateFilename       = "kafka.crt"
 	kafkaPrivateKeyFilename        = "kafka.key"
 	kafkaCAFilename                = "ca.crt"
@@ -243,7 +242,6 @@ func podSpecWithKafka(podSpec *corev1.PodSpec, resourceOptions ResourceOptions) 
 		makeKafkaSecretEnvVar(kafkaSchemaRegistryPasswordKey, resourceOptions.KafkaratorSecretName),
 		makeKafkaSecretEnvVar(kafkaCAKey, resourceOptions.KafkaratorSecretName),
 		makeKafkaSecretEnvVar(kafkaCredStorePasswordKey, resourceOptions.KafkaratorSecretName),
-		makeKafkaSecretEnvVar(KafkaSecretUpdated, resourceOptions.KafkaratorSecretName),
 	}...)
 
 	// Inject path environment variables to refer to mounted secrets
