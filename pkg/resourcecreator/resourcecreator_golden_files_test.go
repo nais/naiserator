8425a2d86db595b67e4f2dccabb832e8c29d530f
Kim Tore Jensen <kimtjen@gmail.com>
2021-06-04 12:52:23 +0200

    test runner for naisjob

--- pkg/resourcecreator/resourcecreator_golden_files_test.go
+++ pkg/resourcecreator/resourcecreator_golden_files_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	nais_io_v1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 	"github.com/nais/naiserator/pkg/test/goldenfile"
@@ -13,15 +14,20 @@ import (
 )
 
 const (
-	testDataDirectory = "testdata"
+	applicationTestDataDirectory = "testdata"
+	naisjobTestDataDirectory     = "testdata/naisjob"
 )
 
 type applicationTestCase struct {
 	Input nais_io_v1alpha1.Application
 }
 
+type naisjobTestCase struct {
+	Input nais_io_v1.Naisjob
+}
+
 func TestApplicationGoldenFile(t *testing.T) {
-	goldenfile.Run(t, testDataDirectory, func(input []byte, resourceOptions resource.Options) (resource.Operations, error) {
+	goldenfile.Run(t, applicationTestDataDirectory, func(input []byte, resourceOptions resource.Options) (resource.Operations, error) {
 		test := applicationTestCase{}
 		err := yaml.Unmarshal(input, &test)
 		if err != nil {
@@ -34,5 +40,22 @@ func TestApplicationGoldenFile(t *testing.T) {
 		}
 
 		return resourcecreator.CreateApplication(&test.Input, resourceOptions)
+	})
+}
+
+func TestNaisjobGoldenFile(t *testing.T) {
+	goldenfile.Run(t, naisjobTestDataDirectory, func(input []byte, resourceOptions resource.Options) (resource.Operations, error) {
+		test := naisjobTestCase{}
+		err := yaml.Unmarshal(input, &test)
+		if err != nil {
+			return nil, err
+		}
+
+		err = test.Input.ApplyDefaults()
+		if err != nil {
+			return nil, fmt.Errorf("apply default values to Application object: %s", err)
+		}
+
+		return resourcecreator.CreateNaisjob(&test.Input, resourceOptions)
 	})
 }
