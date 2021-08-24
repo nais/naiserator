f06764bc8dda2c54e92799f148e1b4728c2fd0c7
Kim Tore Jensen <kimtjen@gmail.com>
2021-08-24 15:50:17 +0200

    wonderwall: provide dummy encryption key

--- pkg/resourcecreator/idporten/sidecar.go
+++ pkg/resourcecreator/idporten/sidecar.go
@@ -1,6 +1,7 @@
 package idporten
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
@@ -36,11 +37,17 @@ func Wonderwall(port int32, targetPort int, wonderwallImage string) corev1.Conta
 				Name:  "WONDERWALL_REDIS",
 				Value: fmt.Sprintf("nais-io-wonderwall-redis:%d", redisPort),
 			},
+			{
+				// FIXME: the encryption key must be securely generated,
+				// then persisted in a secret, and finally referred to.
+				Name:  "WONDERWALL_ENCRYPTION_KEY",
+				Value: base64.StdEncoding.EncodeToString([]byte("0123456789abcdef")),
+			},
 		},
 		Ports: []corev1.ContainerPort{{
 			ContainerPort: port,
 			Protocol:      corev1.ProtocolTCP,
-			Name:          "http",
+			Name:          "redis",
 		}},
 		Resources: pod.ResourceLimits(resourcesSpec),
 		SecurityContext: &corev1.SecurityContext{
