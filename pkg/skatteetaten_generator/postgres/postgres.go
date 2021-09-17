b47e16ae751a9c0996ee371288ab4a8b54d45471
Bjarte S. Karlsen <bjarte.karlsen@skatteetaten.no>
2021-09-17 12:49:03 +0200

    refactor and use nais sin deployment generator (#4)

--- pkg/skatteetaten_generator/postgres/postgres.go
+++ pkg/skatteetaten_generator/postgres/postgres.go
@@ -6,23 +6,29 @@ import (
 	azure_microsoft_com_v1alpha1 "github.com/nais/liberator/pkg/apis/azure.microsoft.com/v1alpha1"
 	skatteetaten_no_v1alpha1 "github.com/nais/liberator/pkg/apis/nebula.skatteetaten.no/v1alpha1"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
+	"github.com/nais/naiserator/pkg/skatteetaten_generator/postgres_env"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-//TODO: where do we want name generation?
-func CreateDatabaseAndUsers(source resource.Source, ast *resource.Ast, pgd []*skatteetaten_no_v1alpha1.PostgreDatabaseConfig, resourceGroup string) {
+// TODO: where do we want name generation?
+func Create(source resource.Source, ast *resource.Ast, pgd []*skatteetaten_no_v1alpha1.PostgreDatabaseConfig, resourceGroup string) {
 
 	// TODO handle updating
-	for _, db := range pgd {
+	for dbIndex, db := range pgd {
 		generatePostgresDatabase(source, ast, resourceGroup, *db)
-		for _, user := range db.Users {
-			 generatePostgresUser(source, ast, resourceGroup, *db, *user)
+		for userIndex, user := range db.Users {
+			if dbIndex == 0 && userIndex == 0 {
+				secretName := fmt.Sprintf("postgresqluser-pgu-%s-%s", source.GetName(), user)
+				dbVars := postgres_env.GenerateDbEnv("SPRING_DATASOURCE", secretName)
+				ast.Env = append(ast.Env, dbVars...)
+			}
+			generatePostgresUser(source, ast, resourceGroup, *db, *user)
 		}
 	}
 
 }
 
-func generatePostgresDatabase(source resource.Source, ast *resource.Ast, rg string, database skatteetaten_no_v1alpha1.PostgreDatabaseConfig)  {
+func generatePostgresDatabase(source resource.Source, ast *resource.Ast, rg string, database skatteetaten_no_v1alpha1.PostgreDatabaseConfig) {
 	objectMeta := resource.CreateObjectMeta(source)
 	objectMeta.Name = fmt.Sprintf("pgd-%s-%s-%s", source.GetNamespace(), source.GetName(), database.Name)
 
@@ -39,13 +45,12 @@ func generatePostgresDatabase(source resource.Source, ast *resource.Ast, rg stri
 	}
 	ast.AppendOperation(resource.OperationCreateIfNotExists, db)
 
-
 }
 
 func generatePostgresUser(source resource.Source, ast *resource.Ast, rg string, database skatteetaten_no_v1alpha1.PostgreDatabaseConfig, user skatteetaten_no_v1alpha1.PostgreDatabaseUser) {
 
 	objectMeta := resource.CreateObjectMeta(source)
-	objectMeta.Name= fmt.Sprintf("pgu-%s-%s", source.GetName(), user.Name)
+	objectMeta.Name = fmt.Sprintf("pgu-%s-%s", source.GetName(), user.Name)
 
 	pgu := &azure_microsoft_com_v1alpha1.PostgreSQLUser{
 		TypeMeta: metav1.TypeMeta{
@@ -54,7 +59,7 @@ func generatePostgresUser(source resource.Source, ast *resource.Ast, rg string, 
 		},
 		ObjectMeta: objectMeta,
 		Spec: azure_microsoft_com_v1alpha1.PostgreSQLUserSpec{
-			DbName:         fmt.Sprintf("pgd-%s-%s-%s", source.GetNamespace(), source.GetName(), database.Name),
+			DbName:        fmt.Sprintf("pgd-%s-%s-%s", source.GetNamespace(), source.GetName(), database.Name),
 			ResourceGroup: rg,
 			Server:        fmt.Sprintf("pgs-%s-%s", source.GetNamespace(), database.Server),
 			Roles:         []string{user.Role},
