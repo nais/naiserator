032a9ae06bfe441da506e67bca56a9d2932b680b
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2021-10-14 11:22:56 +0200

    Concurrency policy (#301)
    * add: ConcurrencyPolicy feature for naisjobs
    
    * changes to cronjob
    
    * use: right version of liberator
    
    * add: concurrency-policy test
    
    * fix: gofmt file
    
    * add: use of latest liberator commit from HEAD
    
    * use new getter for concurrency policy
    
    Co-authored-by: Kim Tore Jensen <kimtjen@gmail.com>

--- pkg/resourcecreator/batch/cronjob.go
+++ pkg/resourcecreator/batch/cronjob.go
@@ -35,6 +35,7 @@ func CreateCronJob(naisjob *nais_io_v1.Naisjob, ast *resource.Ast, resourceOptio
 			},
 			SuccessfulJobsHistoryLimit: util.Int32p(naisjob.Spec.SuccessfulJobsHistoryLimit),
 			FailedJobsHistoryLimit:     util.Int32p(naisjob.Spec.FailedJobsHistoryLimit),
+			ConcurrencyPolicy:          v1beta1.ConcurrencyPolicy(naisjob.GetConcurrencyPolicy()),
 		},
 	}
 
