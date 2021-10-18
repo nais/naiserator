4ffe36b250230b1899dbf723b365fa3a33456033
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-10-18 13:18:40 +0200

    wip

--- pkg/resourcecreator/azure/azureadapplication.go
+++ pkg/resourcecreator/azure/azureadapplication.go
@@ -14,6 +14,7 @@ import (
 	"github.com/nais/naiserator/pkg/resourcecreator/accesspolicy"
 	"github.com/nais/naiserator/pkg/resourcecreator/pod"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
+	"github.com/nais/naiserator/pkg/resourcecreator/wonderwall"
 	"github.com/nais/naiserator/pkg/util"
 )
 
@@ -21,19 +22,31 @@ const (
 	applicationDefaultCallbackPath = "/oauth2/callback"
 )
 
-func adApplication(objectMeta metav1.ObjectMeta, naisAzure nais_io_v1.Azure, naisIngress []nais_io_v1.Ingress, naisAccessPolicy nais_io_v1.AccessPolicy, clusterName string, annotations map[string]string) (*nais_io_v1.AzureAdApplication, error) {
-	replyURLs := naisAzure.Application.ReplyURLs
+type Source interface {
+	resource.Source
+	GetAccessPolicy() nais_io_v1.AccessPolicy
+	GetAzure() nais_io_v1.Azure
+	GetPort() int
+	GetIngress() []nais_io_v1.Ingress
+}
+
+func adApplication(source Source, resourceOptions resource.Options) (*nais_io_v1.AzureAdApplication, error) {
+	replyURLs := source.GetAzure().Application.ReplyURLs
 
 	if len(replyURLs) == 0 {
-		replyURLs = oauthCallbackURLs(naisIngress)
+		replyURLs = oauthCallbackURLs(source.GetIngress())
 	}
 
-	secretName, err := azureSecretName(objectMeta.Name)
+	secretName, err := azureSecretName(source.GetName())
 	if err != nil {
 		return &nais_io_v1.AzureAdApplication{}, err
 	}
 
-	copyAzureAnnotations(annotations, objectMeta.Annotations)
+	objectMeta := resource.CreateObjectMeta(source)
+	copyAzureAnnotations(source.GetAnnotations(), objectMeta.Annotations)
+
+	clusterName := resourceOptions.ClusterName
+	preAuthorizedApps := accesspolicy.InboundRulesWithDefaults(source.GetAccessPolicy().Inbound.Rules, objectMeta.Namespace, clusterName)
 
 	return &nais_io_v1.AzureAdApplication{
 		TypeMeta: metav1.TypeMeta{
@@ -43,12 +56,12 @@ func adApplication(objectMeta metav1.ObjectMeta, naisAzure nais_io_v1.Azure, nai
 		ObjectMeta: objectMeta,
 		Spec: nais_io_v1.AzureAdApplicationSpec{
 			ReplyUrls:                 mapReplyURLs(replyURLs),
-			PreAuthorizedApplications: accesspolicy.InboundRulesWithDefaults(naisAccessPolicy.Inbound.Rules, objectMeta.Namespace, clusterName),
-			Tenant:                    naisAzure.Application.Tenant,
+			PreAuthorizedApplications: preAuthorizedApps,
+			Tenant:                    source.GetAzure().Application.Tenant,
 			SecretName:                secretName,
-			Claims:                    naisAzure.Application.Claims,
-			SinglePageApplication:     naisAzure.Application.SinglePageApplication,
-			AllowAllUsers:             naisAzure.Application.AllowAllUsers,
+			Claims:                    source.GetAzure().Application.Claims,
+			SinglePageApplication:     source.GetAzure().Application.SinglePageApplication,
+			AllowAllUsers:             source.GetAzure().Application.AllowAllUsers,
 		},
 	}, nil
 }
@@ -86,12 +99,12 @@ func azureSecretName(name string) (string, error) {
 	return namegen.SuffixedShortName(prefixedName, suffix, maxLen)
 }
 
-func Create(source resource.Source, ast *resource.Ast, resourceOptions resource.Options, naisAzure nais_io_v1.Azure, naisIngress []nais_io_v1.Ingress, naisAccessPolicy nais_io_v1.AccessPolicy) error {
-	if !resourceOptions.AzureratorEnabled || !naisAzure.Application.Enabled {
+func Create(source Source, ast *resource.Ast, resourceOptions resource.Options) error {
+	if !resourceOptions.AzureratorEnabled || !source.GetAzure().Application.Enabled {
 		return nil
 	}
 
-	azureAdApplication, err := adApplication(resource.CreateObjectMeta(source), naisAzure, naisIngress, naisAccessPolicy, resourceOptions.ClusterName, source.GetAnnotations())
+	azureAdApplication, err := adApplication(source, resourceOptions)
 	if err != nil {
 		return err
 	}
@@ -101,5 +114,28 @@ func Create(source resource.Source, ast *resource.Ast, resourceOptions resource.
 	pod.WithAdditionalSecret(ast, azureAdApplication.Spec.SecretName, nais_io_v1alpha1.DefaultAzureratorMountPath)
 	pod.WithAdditionalEnvFromSecret(ast, azureAdApplication.Spec.SecretName)
 
+	if !resourceOptions.WonderwallEnabled || source.GetAzure().Sidecar == nil || !source.GetAzure().Sidecar.Enabled {
+		return nil
+	}
+
+	config := wonderwallConfig(source, azureAdApplication.Spec.SecretName)
+	err = wonderwall.Create(source, ast, resourceOptions, config)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func wonderwallConfig(source Source, providerSecretName string) wonderwall.Configuration {
+	ingress := string(source.GetIngress()[0])
+
+	return wonderwall.Configuration{
+		AutoLogin:             source.GetAzure().Sidecar.AutoLogin,
+		ErrorPath:             source.GetAzure().Sidecar.ErrorPath,
+		Ingress:               ingress,
+		Provider:              "azure",
+		ProviderSecretName:    providerSecretName,
+		PostLogoutRedirectURI: ingress,
+	}
 }
--- pkg/resourcecreator/wonderwall/wonderwall.go
+++ pkg/resourcecreator/wonderwall/wonderwall.go
@@ -40,8 +40,13 @@ type Configuration struct {
 	PostLogoutRedirectURI string
 }
 
-func Create(app *nais_io_v1alpha1.Application, ast *resource.Ast, resourceOptions resource.Options, cfg Configuration) error {
-	app.Labels["aiven"] = "enabled"
+type Source interface {
+	resource.Source
+	GetPort() int
+}
+
+func Create(source Source, ast *resource.Ast, resourceOptions resource.Options, cfg Configuration) error {
+	source.GetLabels()["aiven"] = "enabled"
 
 	if len(cfg.Provider) == 0 {
 		return fmt.Errorf("configuration has empty provider")
@@ -55,12 +60,12 @@ func Create(app *nais_io_v1alpha1.Application, ast *resource.Ast, resourceOption
 		return fmt.Errorf("configuration has empty ingress")
 	}
 
-	wonderwallSecret, err := sidecarSecret(app, cfg)
+	wonderwallSecret, err := sidecarSecret(source, cfg)
 	if err != nil {
 		return err
 	}
 
-	container, err := sidecarContainer(app, resourceOptions, cfg)
+	container, err := sidecarContainer(source, resourceOptions, cfg)
 	if err != nil {
 		return err
 	}
@@ -77,20 +82,24 @@ func Create(app *nais_io_v1alpha1.Application, ast *resource.Ast, resourceOption
 	return nil
 }
 
-func ShouldEnable(app *nais_io_v1alpha1.Application, resourceOptions resource.Options) bool {
+func ShouldEnable(app *nais_io_v1alpha1.Application, resourceOptions resource.Options) (bool, error) {
 	isGCP := len(resourceOptions.GoogleTeamProjectId) > 0
 
-	if app.Spec.IDPorten == nil || app.Spec.IDPorten.Sidecar == nil || !isGCP {
-		return false
+	idportenDisabled := app.Spec.IDPorten == nil || app.Spec.IDPorten.Sidecar == nil
+	azureDisabled := app.Spec.Azure == nil || app.Spec.Azure.Sidecar == nil
+
+	if idportenDisabled || azureDisabled || !isGCP {
+		return false, nil
 	}
 
 	idPortenEnabled := app.Spec.IDPorten.Enabled && app.Spec.IDPorten.Sidecar.Enabled
+	azureEnabled := app.Spec.Azure.Application.Enabled && app.Spec.Azure.Sidecar.Enabled
 
-	return resourceOptions.DigdiratorEnabled && idPortenEnabled
+	return (resourceOptions.DigdiratorEnabled && idPortenEnabled) || (resourceOptions.AzureratorEnabled && azureEnabled), nil
 }
 
-func sidecarContainer(app *nais_io_v1alpha1.Application, resourceOptions resource.Options, cfg Configuration) (*corev1.Container, error) {
-	targetPort := app.Spec.Port
+func sidecarContainer(source Source, resourceOptions resource.Options, cfg Configuration) (*corev1.Container, error) {
+	targetPort := source.GetPort()
 	resourcesSpec := nais_io_v1.ResourceRequirements{
 		Limits: &nais_io_v1.ResourceSpec{
 			Cpu:    "250m",
@@ -166,7 +175,7 @@ func envVars(cfg Configuration, targetPort int) []corev1.EnvVar {
 	return result
 }
 
-func sidecarSecret(source resource.Source, cfg Configuration) (*corev1.Secret, error) {
+func sidecarSecret(source Source, cfg Configuration) (*corev1.Secret, error) {
 	prefixedName := fmt.Sprintf("%s-wonderwall-%s", cfg.Provider, source.GetName())
 	secretName, err := namegen.ShortName(prefixedName, validation.DNS1123LabelMaxLength)
 	if err != nil {
