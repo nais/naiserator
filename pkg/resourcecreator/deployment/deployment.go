nais/naiserator

commit d515050a3c1434552e475c54a80fdfc0bb7af98d

restricted securitycontext as optin

pkg/resourcecreator/deployment/deployment.go
@@ -62,7 +62,7 @@ func addCleanupLabels(app *nais_io_v1alpha1.Application, meta metav1.ObjectMeta)
 }
 
 func deploymentSpec(app *nais_io_v1alpha1.Application, ast *resource.Ast, resourceOptions resource.Options) (*appsv1.DeploymentSpec, error) {
-	podSpec, err := pod.CreateSpec(ast, resourceOptions, app.Name, corev1.RestartPolicyAlways)
+	podSpec, err := pod.CreateSpec(ast, resourceOptions, app.Name, app.Annotations, corev1.RestartPolicyAlways)
 	if err != nil {
 		return nil, err
 	}
@@ -90,7 +90,7 @@ func deploymentSpec(app *nais_io_v1alpha1.Application, ast *resource.Ast, resour
 	}
 
 	return &appsv1.DeploymentSpec{
-		Replicas: util.Int32p(resourceOptions.NumReplicas),
+		Replicas: util.Int32p(int32(*app.Spec.Replicas.Min)),
 		Selector: &metav1.LabelSelector{
 			MatchLabels: map[string]string{"app": app.Name},
 		},

pkg/resourcecreator/testdata/vanilla_on_premises.yaml
@@ -104,7 +104,7 @@ tests:
           - .spec.template.metadata.creationTimestamp
         resource:
           spec:
-            replicas: 1
+            replicas: 2
             selector:
               matchLabels:
                 app: myapplication

pkg/resourcecreator/batch/job.go
@@ -13,7 +13,7 @@ import (
 
 func CreateJobSpec(naisjob *nais_io_v1.Naisjob, ast *resource.Ast, resourceOptions resource.Options) (batchv1.JobSpec, error) {
 
-	podSpec, err := pod.CreateSpec(ast, resourceOptions, naisjob.GetName(), RestartPolicy(naisjob.Spec.RestartPolicy))
+	podSpec, err := pod.CreateSpec(ast, resourceOptions, naisjob.Name, naisjob.Annotations, RestartPolicy(naisjob.Spec.RestartPolicy))
 	if err != nil {
 		return batchv1.JobSpec{}, err
 	}

pkg/resourcecreator/pod/pod.go
@@ -2,6 +2,7 @@ package pod
 
 import (
 	"fmt"
+	"k8s.io/utils/pointer"
 	"strconv"
 	"strings"
 
@@ -36,7 +37,7 @@ func reorderContainers(appName string, containers []corev1.Container) []corev1.C
 	return reordered
 }
 
-func CreateSpec(ast *resource.Ast, resourceOptions resource.Options, appName string, restartPolicy corev1.RestartPolicy) (*corev1.PodSpec, error) {
+func CreateSpec(ast *resource.Ast, resourceOptions resource.Options, appName string, annotations map[string]string, restartPolicy corev1.RestartPolicy) (*corev1.PodSpec, error) {
 	var err error
 
 	containers := reorderContainers(appName, ast.Containers)
@@ -52,6 +53,17 @@ func CreateSpec(ast *resource.Ast, resourceOptions resource.Options, appName str
 			{Name: "gpr-credentials"},
 			{Name: "ghcr-credentials"},
 		},
+	}
+
+	if restricted(annotations) {
+		podSpec.Containers[0].SecurityContext = &corev1.SecurityContext{
+			RunAsUser:                pointer.Int64Ptr(int64(1069)),
+			RunAsGroup:               pointer.Int64Ptr(int64(1069)),
+			RunAsNonRoot:             pointer.BoolPtr(true),
+			Privileged:               pointer.BoolPtr(false),
+			AllowPrivilegeEscalation: pointer.BoolPtr(false),
+			Capabilities:             &corev1.Capabilities{Drop: []corev1.Capability{"all"}},
+		}
 	}
 
 	if len(resourceOptions.HostAliases) > 0 {
@@ -362,4 +374,13 @@ func leadingSlash(s string) string {
 		return s
 	}
 	return "/" + s
+}
+
+func restricted(annotations map[string]string) bool {
+	val, found := annotations["nais.io/restricted"]
+	if !found {
+		return false
+	}
+
+	return strings.ToLower(val) == "true"
 }

pkg/resourcecreator/testdata/opt_in_securitycontext.yaml
@@ -1,0 +1,44 @@
+config:
+  description: opt-in restricted securitycontext
+
+resourceoptions:
+  NumReplicas: 1
+  Linkerd: true
+  NetworkPolicy: true
+  GoogleProjectID: google-project-id
+
+input:
+  kind: Application
+  apiVersion: v1alpha1
+  metadata:
+    name: myapplication
+    namespace: mynamespace
+    uid: "123456"
+    labels:
+      team: myteam
+    annotations:
+      nais.io/restricted: true
+  spec:
+    image: navikt/myapplication:1.2.3
+tests:
+  - apiVersion: apps/v1
+    kind: Deployment
+    name: myapplication
+    operation: CreateOrUpdate
+    match:
+      - type: subset
+        name: "restricted securitycontext"
+        resource:
+          spec:
+            replicas: 2
+            template:
+              spec:
+                containers:
+                  - securityContext:
+                      runAsUser: 1069
+                      runAsGroup: 1069
+                      allowPrivilegeEscalation: false
+                      runAsNonRoot: true
+                      privileged: false
+                      capabilities:
+                        drop: ["all"]

commit dadeb9e1f81f49fedc222cbf9c618faa61707178

Make sure deployments are patched with correct number of replicas
removes regression introduced in d515050a3c1434552e475c54a80fdfc0bb7af98d

pkg/resourcecreator/deployment/deployment.go
@@ -116,7 +116,7 @@ func deploymentSpec(app Source, ast *resource.Ast, resourceOptions resource.Opti
 	}
 
 	return &appsv1.DeploymentSpec{
-		Replicas: util.Int32p(int32(*app.GetReplicas().Min)),
+		Replicas: util.Int32p(resourceOptions.NumReplicas),
 		Selector: &metav1.LabelSelector{
 			MatchLabels: map[string]string{"app": app.GetName()},
 		},

pkg/synchronizer/rollout.go
@@ -1,6 +1,7 @@
 package synchronizer
 
 import (
+	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 	appsv1 "k8s.io/api/apps/v1"
 )
@@ -14,7 +15,11 @@ type Rollout struct {
 	SynchronizationHash string
 }
 
-// SetCurrentDeployment makes sure newly created Deployment objects matches autoscaling properties of an
+type ReplicaResource interface {
+	GetReplicas() *nais_io_v1.Replicas
+}
+
+// SetNumReplicas makes sure newly created Deployment objects matches autoscaling properties of an
 // existing deployment.
 //
 // If the autoscaler is unavailable when a deployment is made, we risk scaling the application to the default
@@ -23,15 +28,17 @@ type Rollout struct {
 //
 // The number of replicas is set to whichever is highest: the current number of replicas (which might be zero),
 // or the default number of replicas.
-func (r *Rollout) SetCurrentDeployment(deployment *appsv1.Deployment, currentReplicasMin int) {
-	if currentReplicasMin == 0 {
+func (r *Rollout) SetNumReplicas(deployment *appsv1.Deployment, app ReplicaResource) {
+	if *app.GetReplicas().Min == 0 && *app.GetReplicas().Max == 0 {
+		// first, check if an app _should_ be scaled to zero by setting min = max = 0
 		r.ResourceOptions.NumReplicas = 0
 	} else if deployment != nil && deployment.Spec.Replicas != nil {
-		// if a deployment already exists, use that deployment's number of replicas;
-		// unless it is scaled to zero, in which case we increase the number of replicas to the minimum number required.
-		r.ResourceOptions.NumReplicas = max(int32(currentReplicasMin), *deployment.Spec.Replicas)
+		// if a deployment already exists, use that deployment's number of replicas,
+		// unless the minimum allowed replica count is below that of the application spec.
+		r.ResourceOptions.NumReplicas = max(int32(*app.GetReplicas().Min), *deployment.Spec.Replicas)
 	} else {
-		r.ResourceOptions.NumReplicas = int32(currentReplicasMin)
+		// if this is a new deployment, fall back to the lowest number of replicas allowed in the application spec.
+		r.ResourceOptions.NumReplicas = int32(*app.GetReplicas().Min)
 	}
 }
 

pkg/synchronizer/synchronizer.go
@@ -258,10 +258,6 @@ func (n *Synchronizer) Sync(ctx context.Context, rollout Rollout) (bool, error) 
 	return n.rolloutWithRetryAndMetrics(commits)
 }
 
-type ReplicaResource interface {
-	GetReplicas() *nais_io_v1.Replicas
-}
-
 // Prepare converts a NAIS application spec into a Rollout object.
 // This is a read-only operation
 // The Rollout object contains callback functions that commits changes in the cluster.
@@ -305,7 +301,7 @@ func (n *Synchronizer) Prepare(ctx context.Context, app resource.Source, generat
 		if err != nil && !errors.IsNotFound(err) {
 			return nil, fmt.Errorf("query existing deployment: %s", err)
 		}
-		rollout.SetCurrentDeployment(previousDeployment, *rr.GetReplicas().Min)
+		rollout.SetNumReplicas(previousDeployment, rr)
 	}
 
 	// Retrieve current namespace to check for labels and annotations

pkg/resourcecreator/testdata/additional_kernel_capabilities.yaml
@@ -36,7 +36,6 @@ tests:
         name: "restricted securitycontext with additional capabilities"
         resource:
           spec:
-            replicas: 2
             template:
               spec:
                 containers:

pkg/resourcecreator/testdata/vanilla_gcp.yaml
@@ -224,7 +224,7 @@ tests:
         name: "restricted securitycontext"
         resource:
           spec:
-            replicas: 2
+            replicas: 1
             template:
               spec:
                 containers:

pkg/resourcecreator/testdata/vanilla_on_premises.yaml
@@ -105,7 +105,7 @@ tests:
           - .spec.template.metadata.creationTimestamp
         resource:
           spec:
-            replicas: 2
+            replicas: 1
             selector:
               matchLabels:
                 app: myapplication