8693c5f6cd123a2b8417b1644e5535057d08090b
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2022-03-07 10:22:45 +0100

    style(synchronizer): clean up imports

--- pkg/synchronizer/synchronizer.go
+++ pkg/synchronizer/synchronizer.go
@@ -9,9 +9,6 @@ import (
 
 	iam_cnrm_cloud_google_com_v1beta1 "github.com/nais/liberator/pkg/apis/iam.cnrm.cloud.google.com/v1beta1"
 	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
-	generator2 "github.com/nais/naiserator/pkg/event/generator"
-	"github.com/nais/naiserator/pkg/readonly"
-	"github.com/nais/naiserator/pkg/resourcecreator/google"
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -25,9 +22,12 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
+	"github.com/nais/naiserator/pkg/event/generator"
 	"github.com/nais/naiserator/pkg/kafka"
 	"github.com/nais/naiserator/pkg/metrics"
 	"github.com/nais/naiserator/pkg/naiserator/config"
+	"github.com/nais/naiserator/pkg/readonly"
+	"github.com/nais/naiserator/pkg/resourcecreator/google"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 	"github.com/nais/naiserator/updater"
 )
@@ -198,7 +198,7 @@ func (n *Synchronizer) Reconcile(ctx context.Context, req ctrl.Request, app reso
 
 		// Application is not rolled out completely; start monitoring
 		if app.GetStatus().SynchronizationState == nais_io_v1.EventSynchronized {
-			src, ok := app.(generator2.ImageSource)
+			src, ok := app.(generator.ImageSource)
 			if ok {
 				n.MonitorRollout(src, logger)
 			}
@@ -238,7 +238,7 @@ func (n *Synchronizer) Reconcile(ctx context.Context, req ctrl.Request, app reso
 	}
 
 	// Monitor the rollout status so that we can report a successfully completed rollout to NAIS deploy.
-	src, ok := app.(generator2.ImageSource)
+	src, ok := app.(generator.ImageSource)
 	if ok {
 		n.MonitorRollout(src, logger)
 	}
