db77ae4748d24024bdffdf51d8d9ad59b15f7d06
Kyrre Havik <kyrremann@gmail.com>
2022-03-14 19:12:11 +0100

    revert: wip: integration test (174ec26)

--- pkg/test/fixtures/fixtures.go
+++ pkg/test/fixtures/fixtures.go
@@ -1,7 +1,6 @@
 package fixtures
 
 import (
-	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	nais "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -11,7 +10,6 @@ const (
 	ApplicationName      = "myapplication"
 	ApplicationNamespace = "mynamespace"
 	ApplicationTeam      = "myteam"
-	NaisjobName          = "mynaisjob"
 )
 
 // MinimalApplication returns the absolute minimum application that might live in a Kubernetes cluster.
@@ -56,29 +54,4 @@ func MinimalApplication() *nais.Application {
 		panic(err)
 	}
 	return app
-}
-
-// MinimalNaisjob returns the absolute minimum configuration needed to create a full set of Kubernetes resources.
-func MinimalNaisjob() *nais_io_v1.Naisjob {
-	job := &nais_io_v1.Naisjob{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Naisjob",
-			APIVersion: nais.GroupVersion.Identifier(),
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      NaisjobName,
-			Namespace: ApplicationNamespace,
-			Labels: map[string]string{
-				"team": ApplicationTeam,
-			},
-		},
-		Spec: nais_io_v1.NaisjobSpec{
-			Image: "example",
-		},
-	}
-	err := job.ApplyDefaults()
-	if err != nil {
-		panic(err)
-	}
-	return job
 }
