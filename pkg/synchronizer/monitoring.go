c815c9cf736b6417ef937bc7740dfa0929ff6b19
Kyrre Havik <kyrremann@gmail.com>
2022-03-22 09:48:18 +0100

    fix: feil returhåndtering

--- pkg/synchronizer/monitoring.go
+++ pkg/synchronizer/monitoring.go
@@ -171,8 +171,10 @@ func (n *Synchronizer) monitorNaisjob(ctx context.Context, app generator.Monitor
 			logger.Errorf("Monitor rollout: %v", err)
 			return true
 		}
+		return false
 	}
-	return false
+
+	return true
 }
 
 func (n *Synchronizer) monitorApplication(ctx context.Context, app generator.MonitorSource, logger log.Entry, objectKey client.ObjectKey, completion completionState) bool {
