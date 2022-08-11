264daad6d661e8bc002cf96a5350810da8d2ca0b
Trong Huu Nguyen <tronghn@users.noreply.github.com>
2022-08-11 08:21:53 +0200

    ingress: ensure errors are propagated (#334)

--- pkg/resourcecreator/ingress/ingress_test.go
+++ pkg/resourcecreator/ingress/ingress_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
+	"github.com/stretchr/testify/assert"
+
 	"github.com/nais/naiserator/pkg/generators"
+	"github.com/nais/naiserator/pkg/naiserator/config"
 	"github.com/nais/naiserator/pkg/resourcecreator/ingress"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 	"github.com/nais/naiserator/pkg/test/fixtures"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestIngress(t *testing.T) {
@@ -25,6 +27,33 @@ func TestIngress(t *testing.T) {
 			err = ingress.Create(app, ast, opts)
 
 			assert.NotNil(t, err)
+			assert.Len(t, ast.Operations, 0)
+		}
+	})
+
+	t.Run("ingresses without matching ingress class are rejected", func(t *testing.T) {
+		for _, i := range []nais_io_v1.Ingress{"https://baz.foo", "https://bar.foo"} {
+			app := fixtures.MinimalApplication()
+			app.Spec.Ingresses = []nais_io_v1.Ingress{i}
+			ast := resource.NewAst()
+			err := app.ApplyDefaults()
+			assert.NoError(t, err)
+
+			opts := &generators.Options{}
+			opts.Linkerd = true
+			opts.Config.GatewayMappings = []config.GatewayMapping{
+				{
+					DomainSuffix: ".bar",
+					IngressClass: "very-nginx",
+				},
+				{
+					DomainSuffix: ".baz",
+					IngressClass: "something-else",
+				},
+			}
+			err = ingress.Create(app, ast, opts)
+
+			assert.Error(t, err)
 			assert.Len(t, ast.Operations, 0)
 		}
 	})
