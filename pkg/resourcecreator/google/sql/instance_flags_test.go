204d89cba0dd238daf3d47dfa6d1aa630c150e4b
J-K. Solbakken <jksolbakken@gmail.com>
2022-10-07 11:24:55 +0200

    cleaner assertions with testify

--- pkg/resourcecreator/google/sql/instance_flags_test.go
+++ pkg/resourcecreator/google/sql/instance_flags_test.go
@@ -2,6 +2,8 @@ package google_sql
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBoolTrue(t *testing.T) {
@@ -26,79 +28,61 @@ func TestStringWithinEnum(t *testing.T) {
 	flagName := "auto_explain.log_format"
 	flagValue := "json"
 	err := ValidateFlag(flagName, flagValue)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 }
 
 func TestStringNotWithinEnum(t *testing.T) {
 	flagName := "auto_explain.log_format"
 	flagValue := "bogus"
 	err := ValidateFlag(flagName, flagValue)
-	if err == nil {
-		t.Errorf("'%s' is not within spec", flagValue)
-	}
+	assert.Errorf(t, err, "'%s' is not within spec", flagValue)
 }
 
 func TestIntWithinRange(t *testing.T) {
 	flagName := "commit_siblings"
 	flagValue := "2"
 	err := ValidateFlag(flagName, flagValue)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 }
 
 func TestIntNotWithinRange(t *testing.T) {
 	flagName := "commit_siblings"
 	flagValue := "1001"
 	err := ValidateFlag(flagName, flagValue)
-	if err == nil {
-		t.Errorf("'%s' is not within spec", flagValue)
-	}
+	assert.Errorf(t, err, "'%s' is not within spec", flagValue)
 }
 
 func TestFloatWithinRange(t *testing.T) {
 	flagName := "autovacuum_vacuum_scale_factor"
 	flagValue := "10"
 	err := ValidateFlag(flagName, flagValue)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 }
 
 func TestFloatNotWithinRange(t *testing.T) {
 	flagName := "autovacuum_vacuum_scale_factor"
 	flagValue := "-1"
 	err := ValidateFlag(flagName, flagValue)
-	if err == nil {
-		t.Errorf("'%s' is not within spec", flagValue)
-	}
+	assert.Errorf(t, err, "'%s' is not within spec", flagValue)
 }
 
 func TestIsUnitOf(t *testing.T) {
 	flagName := "effective_cache_size"
 	flagValue := "24576"
 	err := ValidateFlag(flagName, flagValue)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
 }
 
 func TestIsNotUnitOf(t *testing.T) {
 	flagName := "effective_cache_size"
 	flagValue := "24277"
 	err := ValidateFlag(flagName, flagValue)
-	if err == nil {
-		t.Errorf("'%s' is not within spec", flagValue)
-	}
+	assert.Errorf(t, err, "'%s' is not within spec", flagValue)
 }
 
 func TestIsNotEmpty(t *testing.T) {
 	flagName := "pglogical.conflict_log_level"
 	flagValue := ""
 	err := ValidateFlag(flagName, flagValue)
-	if err == nil {
-		t.Errorf("'%s' is not within spec", flagValue)
-	}
+	assert.Errorf(t, err, "'%s' is not within spec", flagValue)
 }
