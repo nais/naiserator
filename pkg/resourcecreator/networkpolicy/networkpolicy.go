35c1bce1dd4a227c0531909bba1515ff3f1d2b9d
Sten Røkke <sten.ivar.rokke@nav.no>
2023-05-04 12:52:41 +0200

    chore: remove default allow all policy for prod-gcp

--- pkg/resourcecreator/networkpolicy/networkpolicy.go
+++ pkg/resourcecreator/networkpolicy/networkpolicy.go
@@ -215,63 +215,28 @@ func defaultIngressRules(cfg Config) []networkingv1.NetworkPolicyIngressRule {
 }
 
 func legacyNetpolSpec(appName string, clusterName string) networkingv1.NetworkPolicySpec {
-	// TODO: remove when all clusters are migrated to new network policies
-	if clusterName != "prod-gcp" {
-		return networkingv1.NetworkPolicySpec{
-			PodSelector: *labelSelector("app", appName),
-			PolicyTypes: []networkingv1.PolicyType{
-				networkingv1.PolicyTypeIngress,
-				networkingv1.PolicyTypeEgress,
-			},
-			Ingress: []networkingv1.NetworkPolicyIngressRule{
-				{
-					From: []networkingv1.NetworkPolicyPeer{
-						{
-							NamespaceSelector: labelSelector("linkerd.io/is-control-plane", "true"),
-						},
-					},
-				},
-			},
-			Egress: []networkingv1.NetworkPolicyEgressRule{
-				{
-					To: []networkingv1.NetworkPolicyPeer{{
+	return networkingv1.NetworkPolicySpec{
+		PodSelector: *labelSelector("app", appName),
+		PolicyTypes: []networkingv1.PolicyType{
+			networkingv1.PolicyTypeIngress,
+			networkingv1.PolicyTypeEgress,
+		},
+		Ingress: []networkingv1.NetworkPolicyIngressRule{
+			{
+				From: []networkingv1.NetworkPolicyPeer{
+					{
 						NamespaceSelector: labelSelector("linkerd.io/is-control-plane", "true"),
-					}},
-				},
-			},
-		}
-	} else {
-		return networkingv1.NetworkPolicySpec{
-			PodSelector: *labelSelector("app", appName),
-			PolicyTypes: []networkingv1.PolicyType{
-				networkingv1.PolicyTypeIngress,
-				networkingv1.PolicyTypeEgress,
-			},
-			Ingress: []networkingv1.NetworkPolicyIngressRule{
-				{
-					From: []networkingv1.NetworkPolicyPeer{
-						{
-							NamespaceSelector: labelSelector("linkerd.io/is-control-plane", "true"),
-						},
 					},
 				},
 			},
-			Egress: []networkingv1.NetworkPolicyEgressRule{
-				{
-					To: []networkingv1.NetworkPolicyPeer{{
-						NamespaceSelector: labelSelector("linkerd.io/is-control-plane", "true"),
-					}},
-				},
-				{
-					To: []networkingv1.NetworkPolicyPeer{{
-						IPBlock: &networkingv1.IPBlock{
-							CIDR:   "0.0.0.0/0",
-							Except: []string{"10.6.0.0/15", "172.16.0.0/12", "192.168.0.0/16"},
-						}},
-					},
-				},
+		},
+		Egress: []networkingv1.NetworkPolicyEgressRule{
+			{
+				To: []networkingv1.NetworkPolicyPeer{{
+					NamespaceSelector: labelSelector("linkerd.io/is-control-plane", "true"),
+				}},
 			},
-		}
+		},
 	}
 }
 
