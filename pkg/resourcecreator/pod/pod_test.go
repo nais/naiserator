a3623b56dc979f7eb4e89198b9849d03d72038c2
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-06-06 09:46:46 +0200

    Add the test

---
+++ pkg/resourcecreator/pod/pod_test.go
@@ -0,0 +1,30 @@
+package pod
+
+import "testing"
+
+func TestGenerateNameFromMountPath(t *testing.T) {
+	t.Run("should generate name for normal mount path", func(t *testing.T) {
+		mountPath := "/var/run/my-config_maps"
+		name := generateNameFromMountPath(mountPath)
+		if name != "var-run-my-config_maps" {
+			t.Errorf("expected name to be 'var-run-configmaps', was '%s'", name)
+		}
+	})
+
+	t.Run("should generate name from mount path containing dot", func(t *testing.T) {
+		mountPath := "/var/run/secrets/nais.io/vault"
+		name := generateNameFromMountPath(mountPath)
+		if name != "var-run-secrets-nais-io-vault" {
+			t.Errorf("expected name to be 'var-run-secrets-nais-io-vault', was '%s'", name)
+		}
+	})
+
+	t.Run("should remove trailing and leading special chars", func(t *testing.T) {
+		mountPath := ".var/run/my-config_maps_"
+		name := generateNameFromMountPath(mountPath)
+		if name != "var-run-my-config_maps" {
+			t.Errorf("expected name to be 'var-run-configmaps', was '%s'", name)
+		}
+	})
+
+}
