92d4cead9467c9cf213189b081f8ff976844dc31
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-06-06 11:39:47 +0200

    Make the test table driven

--- pkg/resourcecreator/pod/pod_test.go
+++ pkg/resourcecreator/pod/pod_test.go
@@ -3,28 +3,21 @@ package pod
 import "testing"
 
 func TestGenerateNameFromMountPath(t *testing.T) {
-	t.Run("should generate name for normal mount path", func(t *testing.T) {
-		mountPath := "/var/run/my-config_maps"
-		name := generateNameFromMountPath(mountPath)
-		if name != "var-run-my-config-maps" {
-			t.Errorf("expected name to be 'var-run-my-config-maps', was '%s'", name)
-		}
-	})
+	testCases := []struct {
+		mountPath string
+		expected  string
+	}{
+		{"/var/run/my-config_maps", "var-run-my-config-maps"},
+		{"/var/run/secrets/nais.io/vault", "var-run-secrets-nais-io-vault"},
+		{".var/run/my-config_maps_", "var-run-my-config-maps"},
+	}
 
-	t.Run("should generate name from mount path containing dot", func(t *testing.T) {
-		mountPath := "/var/run/secrets/nais.io/vault"
-		name := generateNameFromMountPath(mountPath)
-		if name != "var-run-secrets-nais-io-vault" {
-			t.Errorf("expected name to be 'var-run-secrets-nais-io-vault', was '%s'", name)
-		}
-	})
-
-	t.Run("should remove trailing and leading special chars", func(t *testing.T) {
-		mountPath := ".var/run/my-config_maps_"
-		name := generateNameFromMountPath(mountPath)
-		if name != "var-run-my-config-maps" {
-			t.Errorf("expected name to be 'var-run-my-config-maps', was '%s'", name)
-		}
-	})
-
+	for _, tc := range testCases {
+		t.Run(tc.mountPath, func(t *testing.T) {
+			name := generateNameFromMountPath(tc.mountPath)
+			if name != tc.expected {
+				t.Errorf("expected name to be '%s', was '%s'", tc.expected, name)
+			}
+		})
+	}
 }
