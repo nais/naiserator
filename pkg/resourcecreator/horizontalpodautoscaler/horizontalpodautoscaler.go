49608744d109080cb16c3242642f1cbfbb3e58fc
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-11-15 15:44:48 +0100

    Do scaling on Kafka lag if set

--- pkg/resourcecreator/horizontalpodautoscaler/horizontalpodautoscaler.go
+++ pkg/resourcecreator/horizontalpodautoscaler/horizontalpodautoscaler.go
@@ -3,10 +3,15 @@ package horizontalpodautoscaler
 import (
 	"github.com/nais/liberator/pkg/apis/nais.io/v1"
 	v2 "k8s.io/api/autoscaling/v2"
+	k8sresource "k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 	"github.com/nais/naiserator/pkg/util"
+)
+
+const (
+	KafkaConsumerLagMetric = "kafka_consumergroup_group_lag"
 )
 
 type Source interface {
@@ -24,11 +29,18 @@ func Create(source Source, ast *resource.Ast) {
 		return
 	}
 
-	cpuThreshold := replicas.CpuThresholdPercentage
+	metricSpecs := make([]v2.MetricSpec, 0)
+
 	if replicas.ScalingStrategy != nil {
 		if replicas.ScalingStrategy.Cpu != nil {
-			cpuThreshold = replicas.ScalingStrategy.Cpu.ThresholdPercentage
+			metricSpecs = append(metricSpecs, createCpuMetricSpec(replicas.ScalingStrategy.Cpu.ThresholdPercentage))
 		}
+
+		if replicas.ScalingStrategy.Kafka != nil {
+			metricSpecs = append(metricSpecs, createKafkaMetricSpec(replicas.ScalingStrategy.Kafka))
+		}
+	} else {
+		metricSpecs = append(metricSpecs, createCpuMetricSpec(replicas.CpuThresholdPercentage))
 	}
 
 	hpa := &v2.HorizontalPodAutoscaler{
@@ -43,21 +55,44 @@ func Create(source Source, ast *resource.Ast) {
 				Kind:       "Deployment",
 				Name:       source.GetName(),
 			},
-			Metrics: []v2.MetricSpec{
-				{
-					Type: v2.ResourceMetricSourceType,
-					Resource: &v2.ResourceMetricSource{
-						Name: "cpu",
-						Target: v2.MetricTarget{
-							Type:               v2.UtilizationMetricType,
-							AverageUtilization: util.Int32p(int32(cpuThreshold)),
-						},
-					},
-				},
-			},
+			Metrics:     metricSpecs,
 			MinReplicas: util.Int32p(int32(*replicas.Min)),
 			MaxReplicas: int32(*replicas.Max),
 		},
 	}
 	ast.AppendOperation(resource.OperationCreateOrUpdate, hpa)
+}
+
+func createKafkaMetricSpec(kafka *nais_io_v1.KafkaScaling) v2.MetricSpec {
+	return v2.MetricSpec{
+		Type: v2.ExternalMetricSourceType,
+		External: &v2.ExternalMetricSource{
+			Metric: v2.MetricIdentifier{
+				Name: KafkaConsumerLagMetric,
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"topic": kafka.Topic,
+						"group": kafka.ConsumerGroup,
+					},
+				},
+			},
+			Target: v2.MetricTarget{
+				Type:         v2.AverageValueMetricType,
+				AverageValue: k8sresource.NewQuantity(int64(kafka.Threshold), k8sresource.DecimalSI),
+			},
+		},
+	}
+}
+
+func createCpuMetricSpec(percentage int) v2.MetricSpec {
+	return v2.MetricSpec{
+		Type: v2.ResourceMetricSourceType,
+		Resource: &v2.ResourceMetricSource{
+			Name: "cpu",
+			Target: v2.MetricTarget{
+				Type:               v2.UtilizationMetricType,
+				AverageUtilization: util.Int32p(int32(percentage)),
+			},
+		},
+	}
 }
--- pkg/resourcecreator/horizontalpodautoscaler/horizontalpodautoscaler_test.go
+++ pkg/resourcecreator/horizontalpodautoscaler/horizontalpodautoscaler_test.go
@@ -1,6 +1,7 @@
 package horizontalpodautoscaler_test
 
 import (
+	"fmt"
 	"testing"
 
 	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
@@ -70,10 +71,10 @@ func TestHorizontalPodAutoscaler(t *testing.T) {
 
 		horizontalpodautoscaler.Create(app, ast)
 		operation := ast.Operations[0]
-		assert.Equal(t, operation.Operation, resource.OperationCreateOrUpdate)
+		assert.Equal(t, resource.OperationCreateOrUpdate, operation.Operation)
 		hpa, ok := operation.Resource.(*v2.HorizontalPodAutoscaler)
 		assert.True(t, ok)
-		assert.Equal(t, hpa.Spec.Metrics[0].Resource.Target.AverageUtilization, util.Int32p(int32(app.Spec.Replicas.CpuThresholdPercentage)))
+		assert.Equal(t, util.Int32p(int32(app.Spec.Replicas.CpuThresholdPercentage)), hpa.Spec.Metrics[0].Resource.Target.AverageUtilization)
 	})
 
 	t.Run("should use value from scalingStrategy.Cpu.ThresholdPercentage when set", func(t *testing.T) {
@@ -93,9 +94,69 @@ func TestHorizontalPodAutoscaler(t *testing.T) {
 
 		horizontalpodautoscaler.Create(app, ast)
 		operation := ast.Operations[0]
-		assert.Equal(t, operation.Operation, resource.OperationCreateOrUpdate)
+		assert.Equal(t, resource.OperationCreateOrUpdate, operation.Operation)
 		hpa, ok := operation.Resource.(*v2.HorizontalPodAutoscaler)
 		assert.True(t, ok)
-		assert.Equal(t, hpa.Spec.Metrics[0].Resource.Target.AverageUtilization, util.Int32p(int32(app.Spec.Replicas.ScalingStrategy.Cpu.ThresholdPercentage)))
+		assert.Equal(t, util.Int32p(int32(app.Spec.Replicas.ScalingStrategy.Cpu.ThresholdPercentage)), hpa.Spec.Metrics[0].Resource.Target.AverageUtilization)
+	})
+
+	t.Run("should add kafka scale metric when scalingStrategy.Kafka is set", func(t *testing.T) {
+		app := fixtures.MinimalApplication()
+		topic := fmt.Sprintf("%s.mytopic", fixtures.ApplicationTeam)
+		app.Spec.Replicas = &nais_io_v1.Replicas{
+			Min:                util.Intp(1),
+			Max:                util.Intp(10),
+			DisableAutoScaling: false,
+			ScalingStrategy: &nais_io_v1.ScalingStrategy{
+				Kafka: &nais_io_v1.KafkaScaling{
+					Topic:         topic,
+					ConsumerGroup: fixtures.ApplicationName,
+					Threshold:     10,
+				},
+			},
+		}
+		ast := resource.NewAst()
+
+		horizontalpodautoscaler.Create(app, ast)
+		operation := ast.Operations[0]
+		assert.Equal(t, resource.OperationCreateOrUpdate, operation.Operation)
+		hpa, ok := operation.Resource.(*v2.HorizontalPodAutoscaler)
+		assert.True(t, ok)
+
+		externalMetric := hpa.Spec.Metrics[0].External
+		assert.Equal(t, horizontalpodautoscaler.KafkaConsumerLagMetric, externalMetric.Metric.Name)
+		actualAvgValue, _ := externalMetric.Target.AverageValue.AsInt64()
+		assert.Equal(t, int64(10), actualAvgValue)
+		matchLabels := externalMetric.Metric.Selector.MatchLabels
+		assert.Equal(t, topic, matchLabels["topic"])
+		assert.Equal(t, fixtures.ApplicationName, matchLabels["group"])
+	})
+
+	t.Run("should add both cpu and kafka metric when both are set", func(t *testing.T) {
+		app := fixtures.MinimalApplication()
+		topic := fmt.Sprintf("%s.mytopic", fixtures.ApplicationTeam)
+		app.Spec.Replicas = &nais_io_v1.Replicas{
+			Min:                util.Intp(1),
+			Max:                util.Intp(10),
+			DisableAutoScaling: false,
+			ScalingStrategy: &nais_io_v1.ScalingStrategy{
+				Cpu: &nais_io_v1.CpuScaling{
+					ThresholdPercentage: 75,
+				},
+				Kafka: &nais_io_v1.KafkaScaling{
+					Topic:         topic,
+					ConsumerGroup: fixtures.ApplicationName,
+					Threshold:     10,
+				},
+			},
+		}
+		ast := resource.NewAst()
+
+		horizontalpodautoscaler.Create(app, ast)
+		operation := ast.Operations[0]
+		assert.Equal(t, resource.OperationCreateOrUpdate, operation.Operation)
+		hpa, ok := operation.Resource.(*v2.HorizontalPodAutoscaler)
+		assert.True(t, ok)
+		assert.Len(t, hpa.Spec.Metrics, 2)
 	})
 }
