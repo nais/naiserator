0e302759711f9fb840ae0f74aa591946ac0f3a9a
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-04-05 15:51:13 +0200

    Check feature flag and team project id before looking up sqlinstance
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>

--- pkg/generators/sqlinstance.go
+++ pkg/generators/sqlinstance.go
@@ -4,22 +4,24 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/nais/liberator/pkg/apis/sql.cnrm.cloud.google.com/v1beta1"
+	sql_cnrm_cloud_google_com_v1beta1 "github.com/nais/liberator/pkg/apis/sql.cnrm.cloud.google.com/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func prepareSqlInstance(ctx context.Context, kube client.Client, key client.ObjectKey, o *Options) error {
-	sqlinstance := &sql_cnrm_cloud_google_com_v1beta1.SQLInstance{}
-	err := kube.Get(ctx, key, sqlinstance)
-	if err != nil {
-		if !errors.IsNotFound(err) {
-			return fmt.Errorf("query existing sqlinstance: %s", err)
+	if len(o.GetGoogleTeamProjectID()) > 0 && o.Config.Features.SqlInstanceInSharedVpc {
+		sqlinstance := &sql_cnrm_cloud_google_com_v1beta1.SQLInstance{}
+		err := kube.Get(ctx, key, sqlinstance)
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				return fmt.Errorf("query existing sqlinstance: %s", err)
+			}
+			o.SqlInstance.exists = false
+		} else {
+			o.SqlInstance.exists = true
+			o.SqlInstance.hasPrivateIp = sqlinstance.Spec.Settings.IpConfiguration.PrivateNetworkRef != nil
 		}
-		o.SqlInstance.exists = false
-	} else {
-		o.SqlInstance.exists = true
-		o.SqlInstance.hasPrivateIp = sqlinstance.Spec.Settings.IpConfiguration.PrivateNetworkRef != nil
 	}
 	return nil
 }
