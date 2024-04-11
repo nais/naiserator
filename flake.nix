47bf13b40ddd7873d817d04afb8df7ff307edf52
Kim Tore Jensen <kimtjen@gmail.com>
2024-04-11 12:42:44 +0200

    Nix build now operational

--- flake.nix
+++ flake.nix
@@ -17,13 +17,30 @@
         "aarch64-darwin" # 64-bit ARM macOS
       ];
 
-      # Helper to provide system-specific attributes
+      # Lambda helper to provide system-specific attributes
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
+
+    # Development environment output
     in
     {
-      # Development environment output
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          pname = "naiserator";
+          version = "0.0.0";
+          src = ./.;
+          # Hash is a product of the contents of all dependencies, aka Go modules
+          vendorHash = "sha256-OkSvMBo+TSRTIaPF6+wCOQ7YiaAoa+eU2ft5Z4E7Fpw=";
+
+          meta = with pkgs.lib; {
+            description = "Naiserator creates a full set of Kubernetes application infrastructure based on a single application spec.";
+            homepage = "https://github.com/nais/naiserator";
+            license = licenses.mit;
+            maintainers = ["kimtore"];
+          };
+        };
+      });
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
