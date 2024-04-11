f4d42af8a9de5d085d7b24521a5c3d66e181a81e
Kim Tore Jensen <kimtjen@gmail.com>
2024-04-11 13:39:44 +0200

    Build a docker image with nix

--- flake.nix
+++ flake.nix
@@ -17,6 +17,21 @@
         "aarch64-darwin" # 64-bit ARM macOS
       ];
 
+			naiseratorPackage = pkgs: pkgs.buildGoModule {
+				pname = "naiserator";
+				version = "0.0.0";
+				src = pkgs.lib.sources.cleanSource ./.;
+				# Hash is a product of the contents of all dependencies, aka Go modules
+				vendorHash = "sha256-OkSvMBo+TSRTIaPF6+wCOQ7YiaAoa+eU2ft5Z4E7Fpw=";
+
+				meta = with pkgs.lib; {
+					description = "Naiserator creates a full set of Kubernetes application infrastructure based on a single application spec.";
+					homepage = "https://github.com/nais/naiserator";
+					license = licenses.mit;
+					maintainers = ["kimtore"];
+				};
+			};
+
       # Lambda helper to provide system-specific attributes
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
@@ -26,21 +41,26 @@
     in
     {
       packages = forAllSystems ({ pkgs }: {
-        default = pkgs.buildGoModule {
-          pname = "naiserator";
-          version = "0.0.0";
-          src = ./.;
-          # Hash is a product of the contents of all dependencies, aka Go modules
-          vendorHash = "sha256-OkSvMBo+TSRTIaPF6+wCOQ7YiaAoa+eU2ft5Z4E7Fpw=";
+        # Build binaries for your system
+				default = naiseratorPackage pkgs;
 
-          meta = with pkgs.lib; {
-            description = "Naiserator creates a full set of Kubernetes application infrastructure based on a single application spec.";
-            homepage = "https://github.com/nais/naiserator";
-            license = licenses.mit;
-            maintainers = ["kimtore"];
-          };
-        };
+        # Build a docker image for your system
+				docker = pkgs.dockerTools.buildImage {
+					name = "naiserator";
+          created = "now";
+          copyToRoot = with pkgs.dockerTools; [
+            usrBinEnv
+            binSh
+            caCertificates
+            fakeNss
+          ];
+
+					config = {
+						Cmd = [ "${naiseratorPackage pkgs}/bin/naiserator" ];
+					};
+				};
       });
+
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
