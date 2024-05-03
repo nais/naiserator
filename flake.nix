9a53c28016edbcc32642ce9f067aa671bf4b1dca
carl hedgren <carl.hedgren@nav.no>
2024-05-03 13:33:27 +0200

    Add kubetools

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
-  description = "gostuff";
+  description = "naiserator";
 
   # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    pile.url = "github:nix-pile/flakes";
+    pile.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, pile }:
     let
       # Systems supported
       allSystems = [
@@ -19,14 +21,26 @@
 
       # Helper to provide system-specific attributes
       forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+        nixpkgs.lib.genAttrs allSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+
+              overlays = [
+                (final: prev: {
+                  kubetools = pile.packages.${system}.default;
+
+                })
+              ];
+            };
+
+          });
     in {
       # Development environment output
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
-          packages = with pkgs; [ go_1_21 gotools gopls go-mockery ];
+          packages = with pkgs; [ go_1_21 gotools gopls go-mockery kubetools ];
         };
       });
     };
