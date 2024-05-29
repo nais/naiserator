4ccca6f87b697b4c720c8643a0208c043fcc4f0e
Kim Tore Jensen <kimtjen@gmail.com>
2024-05-29 09:44:55 +0200

    Don't set deletionPolicy on sqldatabase .spec, it breaks

--- pkg/resourcecreator/google/sql/database.go
+++ pkg/resourcecreator/google/sql/database.go
@@ -14,13 +14,13 @@ func CreateGoogleSQLDatabase(objectMeta metav1.ObjectMeta, instanceName, dbName,
 
 	// This is an annotation, but also a spec field.
 	// Which one has presedence? We set both to be certain.
-	var deletionPolicy string
+	// var deletionPolicy string
 	if !cascadingDelete {
 		// Prevent out-of-band objects from being deleted when the Kubernetes resource is deleted.
 		util.SetAnnotation(&objectMeta, google.DeletionPolicyAnnotation, google.DeletionPolicyAbandon)
-		deletionPolicy = google_sql_crd.DeletionPolicyAbandon
+		// deletionPolicy = google_sql_crd.DeletionPolicyAbandon
 	} else {
-		deletionPolicy = google_sql_crd.DeletionPolicyDelete
+		// deletionPolicy = google_sql_crd.DeletionPolicyDelete
 	}
 
 	return &google_sql_crd.SQLDatabase{
@@ -30,9 +30,11 @@ func CreateGoogleSQLDatabase(objectMeta metav1.ObjectMeta, instanceName, dbName,
 		},
 		ObjectMeta: objectMeta,
 		Spec: google_sql_crd.SQLDatabaseSpec{
-			InstanceRef:    google_sql_crd.InstanceRef{Name: instanceName},
-			ResourceID:     dbName,
-			DeletionPolicy: deletionPolicy,
+			InstanceRef: google_sql_crd.InstanceRef{Name: instanceName},
+			ResourceID:  dbName,
+			// DeletionPolicy is part of the spec, but setting it to anything else than DELETE seems to break with:
+			// {"severity":"error","logger":"sqldatabase-controller","msg":"error applying desired state","error":"summary: \n"}
+			// DeletionPolicy: deletionPolicy,
 		},
 	}
 }
