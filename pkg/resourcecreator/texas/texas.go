497a16b0c99c7053eafdbebee91b5011418a9211
Kyrre Havik <kyrre.havik@nav.no>
2025-03-25 14:34:53 +0100

    upkeep: DRY, og bruk featureflagg

--- pkg/resourcecreator/texas/texas.go
+++ pkg/resourcecreator/texas/texas.go
@@ -173,7 +173,7 @@ func sidecar(source Source, cfg Config, providers Providers) (*corev1.Container,
 
 	// If GCP is unconfigured, it means we are running on-premises, and we need the web proxy config.
 	// Note that we need the web proxy regardless of whether the app has requested one, so we don't care about `.spec.webProxy`.
-	if cfg.IsGCPEnabled() {
+	if !cfg.IsGCPEnabled() {
 		proxyEnvs, err := proxyopts.EnvironmentVariables(cfg)
 		if err != nil {
 			return nil, fmt.Errorf("generate texas webproxy environment variables: %w", err)
