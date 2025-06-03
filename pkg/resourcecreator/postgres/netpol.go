cab54322ba1d2acc5a5f8af33a72643ea5a0d45c
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-06-03 13:09:55 +0200

    Add required netpols for postgres connection pooler
    Co-authored-by: Sten Røkke <sten.ivar.rokke@nav.no>
    Co-authored-by: Karl Heinz <karl.heinz@nav.no>
    Co-authored-by: Leif Gunnar Erlandsen <leif.gunnar.erlandsen@nav.no>

--- pkg/resourcecreator/postgres/netpol.go
+++ pkg/resourcecreator/postgres/netpol.go
@@ -10,6 +10,7 @@ import (
 
 func createNetworkPolicies(source Source, ast *resource.Ast, pgClusterName, pgNamespace string) {
 	createPostgresNetworkPolicy(source, ast, pgClusterName, pgNamespace)
+	createPoolerNetworkPolicy(source, ast, pgClusterName, pgNamespace)
 	createSourceNetworkPolicy(source, ast, pgNamespace)
 }
 
@@ -59,6 +60,79 @@ func createPostgresNetworkPolicy(source Source, ast *resource.Ast, pgClusterName
 						},
 					},
 				},
+				{
+					From: []v1.NetworkPolicyPeer{
+						{
+							PodSelector: &v2.LabelSelector{
+								MatchLabels: map[string]string{
+									"application": "db-connection-pooler",
+									"app":         source.GetName(),
+								},
+							},
+						},
+					},
+				},
+				{
+					From: []v1.NetworkPolicyPeer{
+						{
+							NamespaceSelector: &v2.LabelSelector{
+								MatchLabels: map[string]string{
+									"kubernetes.io/metadata.name": "nais-system",
+								},
+							},
+							PodSelector: &v2.LabelSelector{
+								MatchLabels: map[string]string{
+									"app.kubernetes.io/name": "postgres-operator",
+								},
+							},
+						},
+					},
+				},
+			},
+			PolicyTypes: []v1.PolicyType{
+				v1.PolicyTypeEgress,
+				v1.PolicyTypeIngress,
+			},
+		},
+	}
+
+	ast.AppendOperation(resource.OperationCreateOrUpdate, pgNetpol)
+}
+
+func createPoolerNetworkPolicy(source Source, ast *resource.Ast, pgClusterName string, pgNamespace string) {
+	objectMeta := resource.CreateObjectMeta(source)
+	objectMeta.OwnerReferences = nil
+	objectMeta.Name = fmt.Sprintf("%s-pooler", pgClusterName)
+	objectMeta.Namespace = pgNamespace
+
+	pgNetpol := &v1.NetworkPolicy{
+		ObjectMeta: objectMeta,
+		TypeMeta: v2.TypeMeta{
+			Kind:       "NetworkPolicy",
+			APIVersion: "networking.k8s.io/v1",
+		},
+		Spec: v1.NetworkPolicySpec{
+			PodSelector: v2.LabelSelector{
+				MatchLabels: map[string]string{
+					"application": "db-connection-pooler",
+					"app":         source.GetName(),
+				},
+			},
+			Egress: []v1.NetworkPolicyEgressRule{
+				{
+					To: []v1.NetworkPolicyPeer{
+						{
+							PodSelector: &v2.LabelSelector{
+								MatchLabels: map[string]string{
+									"application": "spilo",
+									"app":         source.GetName(),
+								},
+							},
+						},
+					},
+				},
+			},
+			Ingress: []v1.NetworkPolicyIngressRule{
 				{
 					From: []v1.NetworkPolicyPeer{
 						{
