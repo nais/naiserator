5de3495557f566fa4d7d69f4363edfd4f4dd81cb
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-10-03 14:16:54 +0200

    Revert "Mount optional configmaps containing custom scripts for each extension loaded"
    This reverts commit 6242c519b72e0462a5d80f3c8a24d3d2bd401bc7.
    
    This feature is not needed considering the thing we wanted to use it for isn't possible for other reasons.

--- pkg/resourcecreator/postgres/postgres.go
+++ pkg/resourcecreator/postgres/postgres.go
@@ -173,23 +173,6 @@ func CreateClusterSpec(source Source, ast *resource.Ast, cfg Config, pgClusterNa
 		extensions[extension] = defaultSchema
 	}
 
-	additionalVolumes := make([]acid_zalan_do_v1.AdditionalVolume, 0, len(extensions))
-	for extension, _ := range extensions {
-		name := fmt.Sprintf("postgres-%s-scripts", extension)
-		additionalVolumes = append(additionalVolumes, acid_zalan_do_v1.AdditionalVolume{
-			Name:      name,
-			MountPath: fmt.Sprintf("/scripts/%s", extension),
-			VolumeSource: corev1.VolumeSource{
-				ConfigMap: &corev1.ConfigMapVolumeSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: name,
-					},
-					Optional: ptr.To(true),
-				},
-			},
-		})
-	}
-
 	collation := "en_US.UTF-8"
 	if postgres.Database != nil && postgres.Database.Collation != "" {
 		collation = fmt.Sprintf("%s.UTF-8", postgres.Database.Collation)
@@ -202,7 +185,6 @@ func CreateClusterSpec(source Source, ast *resource.Ast, cfg Config, pgClusterNa
 		},
 		ObjectMeta: objectMeta,
 		Spec: acid_zalan_do_v1.PostgresSpec{
-			AdditionalVolumes:             additionalVolumes,
 			EnableConnectionPooler:        ptr.To(true),
 			EnableReplicaConnectionPooler: ptr.To(false),
 			ConnectionPooler: &acid_zalan_do_v1.ConnectionPooler{
